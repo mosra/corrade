59a2056532d68391a382b4454f7e9e3283292c97
Vladimír Vondruš <mosra@centrum.cz>
2010-09-01 18:00:42 +0200

    CMake macro for adding multi-file test.

--- modules/Map2XMacros.cmake
+++ modules/Map2XMacros.cmake
@@ -28,3 +28,30 @@ function(map2x_add_test test_name moc_header source_file)
     target_link_libraries(${test_name} ${libraries} ${QT_QTCORE_LIBRARY} ${QT_QTTEST_LIBRARY})
     add_test(${test_name} ${test_name})
 endfunction()
+
+#
+# Function for adding QtTest unit test with multiple source files
+#
+# Useful when there is need to compile more than one cpp/h file into the test.
+#
+# Example:
+#       set(test_headers ComplexTest.h MyObject.h)
+#       set(test_sources ComplexTest.cpp MyObject.cpp)
+#       map2x_add_test(MyComplexTest test_headers test_sources CoreLibrary AnotherLibrary)
+#
+# is expanded to:
+#       qt4_wrap_cpp(MyComplexTest_MOC ComplexTest.h MyObject.h)
+#       add_executable(MyComplexTest ComplexTest.cpp MyObject.cpp ${MyComplexTest_MOC})
+#       target_link_libraries(MyComplexTest CoreLibrary AnotherLibrary ${QT_QTCORE_LIBRARY} ${QT_QTTEST_LIBRARY})
+#       add_test(MyComplexTest MyComplexTest)
+#
+function(map2x_add_multifile_test test_name moc_headers_variable source_files_variable)
+    foreach(library ${ARGN})
+        set(libraries ${library} ${libraries})
+    endforeach()
+
+    qt4_wrap_cpp(${test_name}_MOC ${${moc_headers_variable}})
+    add_executable(${test_name} ${${source_files_variable}} ${${test_name}_MOC})
+    target_link_libraries(${test_name} ${libraries} ${QT_QTCORE_LIBRARY} ${QT_QTTEST_LIBRARY})
+    add_test(${test_name} ${test_name})
+endfunction()
