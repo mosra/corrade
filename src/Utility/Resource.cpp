d8a03bb59f22e2b98954974aacb2e9d180471da2
Vladimír Vondruš <mosra@centrum.cz>
2010-09-06 01:15:36 +0200

    Class for managing compiled-in data resources.

---
+++ src/Utility/Resource.cpp
@@ -0,0 +1,166 @@
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include "Resource.h"
+
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+namespace Map2X { namespace Utility {
+
+map<string, map<string, Resource::ResourceData> > Resource::resources;
+
+void Resource::registerData(const char* group, size_t count, const unsigned char* positions, const unsigned char* filenames, const unsigned char* data) {
+    if(resources.find(group) == resources.end()) resources.insert(pair<string, map<string, ResourceData> >(group, map<string, ResourceData>()));
+
+    /* Cast to type which can be eaten by std::string constructor */
+    const char* _positions = reinterpret_cast<const char*>(positions);
+    const char* _filenames = reinterpret_cast<const char*>(filenames);
+
+    size_t size = sizeof(size_t);
+    size_t oldFilenamePosition = 0, oldDataPosition = 0;
+
+    /* Every 2*sizeof(size_t) is one data */
+    for(size_t i = 0; i != count*2*size; i=i+2*size) {
+        size_t filenamePosition = numberFromString<size_t>(string(_positions+i, size));
+        size_t dataPosition = numberFromString<size_t>(string(_positions+i+size, size));
+
+        ResourceData res;
+        res.data = data;
+        res.position = oldDataPosition;
+        res.size = dataPosition-oldDataPosition;
+
+        string filename = string(_filenames+oldFilenamePosition, filenamePosition-oldFilenamePosition);
+        resources[group].insert(pair<string, ResourceData>(filename, res));
+
+        oldFilenamePosition = filenamePosition;
+        oldDataPosition = dataPosition;
+    }
+}
+
+void Resource::unregisterData(const char* group, const unsigned char* data) {
+    if(resources.find(group) == resources.end()) return;
+
+    /* Positions which to remove */
+    vector<string> positions;
+
+    for(map<string, ResourceData>::iterator it = resources[group].begin(); it != resources[group].end(); ++it) {
+        if(it->second.data == data)
+            positions.push_back(it->first);
+    }
+
+    for(vector<string>::const_iterator it = positions.begin(); it != positions.end(); ++it)
+        resources[group].erase(*it);
+
+    if(resources[group].empty()) resources.erase(group);
+}
+
+string Resource::compile(const string& name, const map<string, string>& files) const {
+    string positions, filenames, data;
+    size_t filenamesLen = 0, dataLen = 0;
+
+    /* Convert data to hexacodes */
+    for(map<string, string>::const_iterator it = files.begin(); it != files.end(); ++it) {
+        filenamesLen += it->first.size();
+        dataLen += it->second.size();
+
+        positions += hexcode(numberToString(filenamesLen));
+        positions += hexcode(numberToString(dataLen));
+
+        filenames += hexcode(it->first, it->first);
+        data += hexcode(it->second, it->first);
+    }
+
+    /* Remove last comma from data */
+    positions = positions.substr(0, positions.size()-2);
+    filenames = filenames.substr(0, filenames.size()-2);
+    data = data.substr(0, data.size()-2);
+
+    /* Resource count */
+    ostringstream count;
+    count << files.size();
+
+    /* Return C++ file */
+    return "/* Compiled resource file. DO NOT EDIT! */\n\n"
+        "#include \"Utility/utilities.h\"\n"
+        "#include \"Utility/Resource.h\"\n\n"
+        "static const unsigned char resourcePositions[] = {\n" +
+        positions + "\n};\n\n"
+        "static const unsigned char resourceFilenames[] = {\n" +
+        filenames + "\n};\n\n"
+        "static const unsigned char resourceData[] = {\n" +
+        data +      "\n};\n\n"
+        "inline int resourceInitializer_" + name + "() {\n"
+        "    Map2X::Utility::Resource::registerData(\"" + group + "\", " + count.str() + ", resourcePositions, resourceFilenames, resourceData);\n"
+        "    return 1;\n"
+        "} AUTOMATIC_INITIALIZER(resourceInitializer_" + name + ")\n\n"
+        "inline int resourceFinalizer_" + name + "() {\n"
+        "    Map2X::Utility::Resource::unregisterData(\"" + group + "\", resourceData);\n"
+        "    return 1;\n"
+        "} AUTOMATIC_FINALIZER(resourceFinalizer_" + name + ")\n";
+}
+
+string Resource::compile(const string& name, const string& filename, const string& data) const {
+    std::map<std::string, std::string> files;
+    files.insert(std::pair<std::string, std::string>(filename, data));
+    return compile(name, files);
+}
+
+string Resource::get(const std::string& filename) const {
+    /* If the group/filename doesn't exist, return empty string */
+    if(resources.find(group) == resources.end() || resources[group].find(filename) == resources[group].end()) return "";
+
+    const ResourceData& r = resources[group][filename];
+    return string(reinterpret_cast<const char*>(r.data)+r.position, r.size);
+}
+
+string Resource::hexcode(const string& data, const string& comment) const {
+    /* Add comment, if set */
+    string output = "    ";
+    if(!comment.empty()) output = "\n    /* " + comment + " */\n" + output;
+
+    int row_len = 4;
+    for(size_t i = 0; i != data.size(); ++i) {
+
+        /* Every row is indented by four spaces and is max 80 characters long */
+        if(row_len > 74) {
+            output += "\n    ";
+            row_len = 4;
+        }
+
+        /* Convert char to hex */
+        ostringstream converter;
+        converter << std::hex;
+        converter << static_cast<unsigned int>(static_cast<unsigned char>(data[i]));
+
+        /* Append to output */
+        output += "0x" + converter.str() + ",";
+        row_len += 3+converter.str().size();
+    }
+
+    return output + '\n';
+}
+
+template<class T> string Resource::numberToString(const T& number) {
+    return string(reinterpret_cast<const char*>(&number), sizeof(T));
+}
+
+template<class T> T Resource::numberFromString(const std::string& number) {
+    return *reinterpret_cast<const T*>(number.c_str());
+}
+
+}}
---
+++ src/Utility/Resource.h
@@ -0,0 +1,162 @@
+#ifndef Map2X_Utility_Resource_h
+#define Map2X_Utility_Resource_h
+/*
+    Copyright © 2007, 2008, 2009, 2010 Vladimír Vondruš <mosra@centrum.cz>
+
+    This file is part of Map2X.
+
+    Map2X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    Map2X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class Map2X::Utility::Resource
+ */
+
+#include <string>
+#include <map>
+
+namespace Map2X { namespace Utility {
+
+/**
+ * @brief Data resource management
+ *
+ * This class provides support for compiled-in data resources - both compiling
+ * and reading.
+ *
+ * Resources can be differentiated into more groups, every resource in given
+ * group has unique filename.
+ *
+ * See also @ref rc.cpp.
+ */
+class Resource {
+    public:
+        /**
+         * @brief Register data resource
+         * @param group         Group name
+         * @param count         File count
+         * @param positions     Positions of filenames and data in binary blobs
+         * @param filenames     Pointer to binary blob with filenames
+         * @param data          Pointer to binary blob with file data
+         *
+         * This function is used internally for automatic data resource
+         * registering, no need to use it directly.
+         */
+        static void registerData(const char* group, size_t count, const unsigned char* positions, const unsigned char* filenames, const unsigned char* data);
+
+        /**
+         * @brief Unregister data resource
+         * @param group         Group name
+         * @param data          Pointer to binary blob with file data
+         *
+         * This function is used internally for automatic data resource
+         * unregistering, no need to use it directly.
+         */
+        static void unregisterData(const char* group, const unsigned char* data);
+
+        /**
+         * @brief Constructor
+         * @param _group        Group name for getting data or compiling new
+         *      resources.
+         */
+        inline Resource(const std::string& _group): group(_group) {}
+
+        /**
+         * @brief Compile data resource file
+         * @param name          Resource name (see RESOURCE_INITIALIZE())
+         * @param files         Map with files, first item of pair is filename,
+         *      second is file data.
+         *
+         * Produces C++ file with hexadecimally represented file data. The file
+         * then must be compiled directly to executable or library.
+         */
+        std::string compile(const std::string& name, const std::map<std::string, std::string>& files) const;
+
+        /**
+         * @brief Compile data resource file
+         * @param name          Resource name (see RESOURCE_INITIALIZE())
+         * @param filename      Filename
+         * @param data          File data
+         *
+         * Convenience function for compiling resource with only one file.
+         */
+        std::string compile(const std::string& name, const std::string& filename, const std::string& data) const;
+
+        /**
+         * @brief Get data resource
+         * @param filename      Filename
+         * @return Data of given group (specified in constructor) and filename.
+         *      Returns empty string if nothing was found.
+         */
+        std::string get(const std::string& filename) const;
+
+    private:
+        struct ResourceData {
+            size_t position;
+            size_t size;
+            const unsigned char* data;
+        };
+
+        static std::map<std::string, std::map<std::string, ResourceData> > resources;
+
+        std::string group;
+
+        std::string hexcode(const std::string& data, const std::string& comment = "") const;
+
+        /** @todo Move to utilities.h? */
+        template<class T> static std::string numberToString(const T& number);
+        template<class T> static T numberFromString(const std::string& number);
+};
+
+/**
+ * @brief Initialize resource
+ *
+ * If a resource is compiled into dynamic library or directly into executable,
+ * it will be initialized automatically thanks to AUTOMATIC_INITIALIZER()
+ * macros. However, if the resource is compiled into static library, it must be
+ * explicitly initialized at via this macro, e.g. at the beginning of main().
+ * You can also wrap these macro calls into another function (which will then
+ * be compiled into dynamic library or main executable) and use
+ * AUTOMATIC_INITIALIZER() macro for automatic call.
+ * @attention This macro should be called outside of any namespace. If you are
+ * running into linker errors with @c resourceInitializer_*, this could be the
+ * problem. If you are in a namespace and cannot call this macro from main(),
+ * try this:
+ * @code
+ * void initialize() {
+ *     RESOURCE_INITIALIZE(res)
+ * }
+ *
+ * namespace Foo {
+ *     void bar() {
+ *         initialize();
+ *
+ *         //...
+ *     }
+ * }
+ * @endcode
+ */
+#define RESOURCE_INITIALIZE(name)                                             \
+    extern int resourceInitializer_##name();                                  \
+    resourceInitializer_##name();
+
+/**
+ * @brief Cleanup resource
+ *
+ * Cleans up previously (even automatically) initialized resource.
+ * @attention This macro should be called outside of any namespace. See
+ * RESOURCE_INITIALIZE() documentation for more information.
+ */
+#define RESOURCE_CLEANUP(name)                                                \
+    extern int resourceInitializer_##name();                                  \
+    resourceFinalizer_##name();
+
+}}
+
+#endif
