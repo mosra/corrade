9d056a6ad68ba45fe180765acc3751b21ee283c0
Vladimír Vondruš <mosra@centrum.cz>
2011-01-22 21:54:38 +0100

    Doc++

--- src/PluginManager/PluginMetadata.h
+++ src/PluginManager/PluginMetadata.h
@@ -29,10 +29,47 @@
 namespace Kompas { namespace PluginManager {
 
 /**
- * @brief %Plugin metadata
- *
- * This class stores metadata about particular plugin.
- * See also @ref PluginManagement.
+@brief %Plugin metadata
+
+This class stores metadata about particular plugin. The plugin metadata are
+stored in plugin configuration file, which resides either besides the dynamic
+plugin binary in a filesystem or is compiled directly into executable with an
+static plugin. See @ref PluginManagement for tutorial and brief introduction
+into how plugins work.
+
+The plugin configuration file has an simple syntax (see Configuration class
+documentation for full specifications). The file stores plugin name,
+description, list of authors, plugin version, list of dependencies (if the
+plugin depends on another) and list of replaced plugins (if the plugin can
+replace plugin and provide the same or better functionality). Plugin name and
+description can be translated into more languages just by adding a new subgroup
+named after that language locale code. Full featured example:
+<pre># Plugin version
+version=1.0
+
+# List of authors
+author=Vladimír Vondruš &lt;mosra@centrum.cz&gt;
+author=Some Random John Doe &lt;john@doe.net&gt;
+
+# Dependencies
+depends=SomeRandomJohnDoesPlugin
+depends=BaseMatrixPlugin
+depends=SkyNetPlugin
+
+# Replaced plugins
+replaces=CrashingMatrixPlugin
+replaces=AlphaMatrixPlugin
+
+# Metadata for default language (English)
+[metadata]
+name=Good Matrix
+description=My first matrix without bugs
+
+# Metadata in Czech
+[metadata/cs_CZ]
+name=Dobrý Matrix
+description=Můj první Matrix bez chyb v Matrixu
+</pre>
  */
 class PLUGINMANAGER_EXPORT PluginMetadata {
     friend class AbstractPluginManager;
