d921c0b47e6c6c9f064eef4ee809ef928a5618be
Vladimír Vondruš <mosra@centrum.cz>
2012-07-31 16:59:25 +0200

    Don't misuse FindCorrade.cmake for setting internal variables.

--- modules/FindCorrade.cmake
+++ modules/FindCorrade.cmake
@@ -14,12 +14,8 @@
 #  CORRADE_PLUGINMANAGER_LIBRARY    - Corrade Plugin manager library
 #  CORRADE_RC_EXECUTABLE            - Corrade resource compiler executable
 #
-# Additionally these variables are defined for internal usage:
-#
-#  CORRADE_BINARY_INSTALL_DIR       - Binary installation directory
-#  CORRADE_LIBRARY_INSTALL_DIR      - Library installation directory
-#  CORRADE_CMAKE_MODULE_INSTALL_DIR - Installation dir for CMake modules
-#  CORRADE_INCLUDE_INSTALL_DIR      - Include installation directory for Corrade headers
+# If Corrade library is found, modules CorradeMacros and CorradeLibSuffix are
+# included.
 #
 
 # Libraries
@@ -41,14 +37,10 @@ find_package_handle_standard_args(Corrade DEFAULT_MSG
     CORRADE_PLUGINMANAGER_LIBRARY
     CORRADE_RC_EXECUTABLE)
 
-if(CORRADE_FOUND)
-    include(CorradeMacros)
-
-    include(CorradeLibSuffix)
-    set_parent_scope(CORRADE_BINARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/bin)
-    set_parent_scope(CORRADE_LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX})
-    set_parent_scope(CORRADE_CMAKE_MODULE_INSTALL_DIR ${CMAKE_ROOT}/Modules)
-    set_parent_scope(CORRADE_INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/include/Corrade)
-
-    set_parent_scope(CORRADE_LIBRARIES ${CORRADE_UTILITY_LIBRARY} ${CORRADE_PLUGINMANAGER_LIBRARY})
+if(NOT CORRADE_FOUND)
+    return()
 endif()
+
+include(CorradeMacros)
+include(CorradeLibSuffix)
+set(CORRADE_LIBRARIES ${CORRADE_UTILITY_LIBRARY} ${CORRADE_PLUGINMANAGER_LIBRARY})
