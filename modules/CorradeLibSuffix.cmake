e16d61f3a1d70ac7ec5b19c91b172e50bd76ab90
Vladimír Vondruš <mosra@centrum.cz>
2012-08-04 19:48:51 +0200

    Put value of LIB_SUFFIX into cache, so it's not detected every time.

--- modules/CorradeLibSuffix.cmake
+++ modules/CorradeLibSuffix.cmake
@@ -1,6 +1,4 @@
-include(CorradeMacros)
-
-if(NOT DEFINED LIB_SUFFIX AND NOT __LIB_SUFFIX_SET)
+if(NOT DEFINED LIB_SUFFIX)
     message(STATUS "LIB_SUFFIX variable is not defined. It will be autodetected now.")
     message(STATUS "You can set it manually with -DLIB_SUFFIX=<value> (64 for example)")
 
@@ -9,20 +7,19 @@ if(NOT DEFINED LIB_SUFFIX AND NOT __LIB_SUFFIX_SET)
         # If there is /usr/lib64 and is not /usr/lib32, set suffix to 64
         # Ubuntu 64bit symlinks /usr/lib64 to /usr/lib, install to /usr/lib there
         if(IS_DIRECTORY /usr/lib64 AND NOT IS_SYMLINK /usr/lib64)
-            set_parent_scope(LIB_SUFFIX 64)
+            set(LIB_SUFFIX 64)
         elseif(IS_DIRECTORY /usr/lib)
-            set_parent_scope(LIB_SUFFIX "")
+            set(LIB_SUFFIX "")
         else()
             message(WARNING "LIB_SUFFIX cannot be autodetected. No /usr/lib neither /usr/lib64 found.")
-            set_parent_scope(LIB_SUFFIX "")
+            set(LIB_SUFFIX "")
         endif()
     else()
-        set_parent_scope(LIB_SUFFIX "")
+        set(LIB_SUFFIX "")
     endif()
 
-    # Workaround for another CMake bug: When LIB_SUFFIX is empty,
-    # set(LIB_SUFFIX "" PARENT_SCOPE) doesn't set anything.
-    set_parent_scope(__LIB_SUFFIX_SET True)
+    # Put the value into cache
+    set(LIB_SUFFIX "${LIB_SUFFIX}" CACHE STRING "Library directory suffix (e.g. 64 for /usr/lib64).")
 
     message(STATUS "LIB_SUFFIX autodetected as '${LIB_SUFFIX}', libraries will be installed into ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}")
 endif()
