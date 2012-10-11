d04a1e7159ed2d67bea35e77a0cc2414b989754c
Vladimír Vondruš <mosra@centrum.cz>
2012-10-11 19:50:55 +0200

    @todo++

--- src/Utility/rc.cpp
+++ src/Utility/rc.cpp
@@ -28,6 +28,8 @@ See @ref resource-management for brief introduction.
 
 @todo Test it
 @todo Check empty files
+@todo Write to file (and don't create any on error)
+@todo Use Debug classes
 */
 
 #include <iostream>
