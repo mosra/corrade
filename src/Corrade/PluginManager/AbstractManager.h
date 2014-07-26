7d614ede0cccc7d9657433597a0dcc45b9f7d939
Vladimír Vondruš <mosra@centrum.cz>
2014-07-26 18:40:34 +0200

    PluginManager: minor cleanup.

--- src/Corrade/PluginManager/AbstractManager.h
+++ src/Corrade/PluginManager/AbstractManager.h
@@ -430,10 +430,10 @@ class CORRADE_PLUGINMANAGER_EXPORT AbstractManager {
            be uninitalized when accessed from CORRADE_PLUGIN_REGISTER(). */
         CORRADE_PLUGINMANAGER_LOCAL static std::vector<StaticPlugin*>*& staticPlugins();
 
-        std::map<std::string, std::vector<AbstractPlugin*> > instances;
-
         CORRADE_PLUGINMANAGER_LOCAL void registerInstance(std::string plugin, AbstractPlugin& instance, const PluginMetadata*& metadata);
         CORRADE_PLUGINMANAGER_LOCAL void unregisterInstance(const std::string& plugin, AbstractPlugin& instance);
+
+        std::map<std::string, std::vector<AbstractPlugin*> > instances;
 };
 
 /** @hideinitializer
