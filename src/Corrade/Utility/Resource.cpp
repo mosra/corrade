5cdb08a956b230dfb6e68179c7e7495264a1d7cf
Vladimír Vondruš <mosra@centrum.cz>
2015-09-08 13:17:54 +0200

    Utility: remove old std::to_string()-related MinGW workarounds.

--- src/Corrade/Utility/Resource.cpp
+++ src/Corrade/Utility/Resource.cpp
@@ -194,7 +194,7 @@ std::string Resource::compile(const std::string& name, const std::string& group,
     if(!files.back().second.empty())
         data.resize(data.size()-1);
 
-    #if defined(CORRADE_TARGET_NACL_NEWLIB) || defined(CORRADE_TARGET_ANDROID) || defined(__MINGW32__)
+    #if defined(CORRADE_TARGET_NACL_NEWLIB) || defined(CORRADE_TARGET_ANDROID)
     std::ostringstream converter;
     converter << files.size();
     #endif
@@ -220,7 +220,7 @@ std::string Resource::compile(const std::string& name, const std::string& group,
         "int resourceInitializer_" + name + "();\n"
         "int resourceInitializer_" + name + "() {\n"
         "    Corrade::Utility::Resource::registerData(\"" + group + "\", " +
-            #if !defined(CORRADE_TARGET_NACL_NEWLIB) && !defined(CORRADE_TARGET_ANDROID) && !defined(__MINGW32__)
+            #if !defined(CORRADE_TARGET_NACL_NEWLIB) && !defined(CORRADE_TARGET_ANDROID)
             std::to_string(files.size()) +
             #else
             converter.str() +
