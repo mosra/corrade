7c47f1b61e07102562cab21630de61fc39e93cb4
Vladimír Vondruš <mosra@centrum.cz>
2016-01-28 15:22:52 +0100

    package: added Homebrew package.

---
+++ package/homebrew/corrade.rb
@@ -0,0 +1,18 @@
+# kate: indent-width 2;
+
+class Corrade < Formula
+  desc "C++11/C++14 multiplatform utility library"
+  homepage "https://github.com/mosra/corrade"
+  head "git://github.com/mosra/corrade.git"
+
+  depends_on "cmake"
+
+  def install
+    system "mkdir build"
+    cd "build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+end
