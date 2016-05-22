b276078f02bb95e9fe2f95704f36e3254cd6e65f
Vladimír Vondruš <mosra@centrum.cz>
2016-05-22 13:09:32 +0200

    package/ci: build for iOS Simulator on Travis.
    Added third entry to the build matrix, moved the commands to separate
    shell scripts and running one of them based on what the actual matrix
    is. The tests are now outputting everything, because sometimes one might
    want to see the actual output (colors, detected values, visual checks).

---
+++ package/ci/travis-desktop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
+    -DBUILD_TESTS=ON \
+    -DCMAKE_BUILD_TYPE=Release
+make -j install
+CORRADE_TEST_COLOR=ON ctest -V
+cd ..
+
+# Examples
+mkdir build-examples && cd build-examples
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$HOME/deps ../src/examples
+make -j
