6e3334bcc6b610397530723446835a79cfa17b3d
Vladimír Vondruš <mosra@centrum.cz>
2016-06-17 19:46:39 +0200

    package/ci: build for Emscripten on Travis instead of AppVeyor.
    Faster.

---
+++ package/ci/travis-emscripten.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ev
+
+git submodule update --init
+
+# Build native corrade-rc
+mkdir build && cd build || exit /b
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DCMAKE_INSTALL_RPATH=$HOME/deps-native/lib \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF
+make -j install
+cd ..
+
+# Crosscompile
+mkdir build-emscripten && cd build-emscripten
+cmake .. \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten.cmake" \
+    -DEMSCRIPTEN_PREFIX=$(echo /usr/local/Cellar/emscripten/*/libexec) \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O1" \
+    -DBUILD_TESTS=ON
+make -j4
+
+# Test
+CORRADE_TEST_COLOR=ON ctest -V
