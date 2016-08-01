ee4bbeb2e4c1003d7bbed4821ea074e8857f7b34
Vladimír Vondruš <mosra@centrum.cz>
2016-08-01 13:47:57 +0200

    package/ci: enable Coveralls.io report on Linux Travis CI build.

--- package/ci/travis-desktop.sh
+++ package/ci/travis-desktop.sh
@@ -6,7 +6,8 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
     -DBUILD_TESTS=ON \
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="--coverage $CXXFLAGS"
 make -j install
 CORRADE_TEST_COLOR=ON ctest -V
 cd ..
