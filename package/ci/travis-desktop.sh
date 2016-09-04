c0fb35c6acfa311b8d6ac00787ff0b51fa2fc9ea
Vladimír Vondruš <mosra@centrum.cz>
2016-09-04 12:21:34 +0200

    package/ci: will the coverage be larger if I build a Debug configuration?

--- package/ci/travis-desktop.sh
+++ package/ci/travis-desktop.sh
@@ -6,12 +6,12 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
     -DBUILD_TESTS=ON \
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Debug
 make -j install
 CORRADE_TEST_COLOR=ON ctest -V
 cd ..
 
 # Examples
 mkdir build-examples && cd build-examples
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$HOME/deps ../src/examples
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$HOME/deps ../src/examples
 make -j
