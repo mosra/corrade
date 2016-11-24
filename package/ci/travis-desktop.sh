8f61c76a9e78e52042e2e24d6fd64e49fa9d8033
Vladimír Vondruš <mosra@centrum.cz>
2016-11-24 13:04:58 +0100

    package/ci: added AddressSanitizer build on Travis.

--- package/ci/travis-desktop.sh
+++ package/ci/travis-desktop.sh
@@ -2,16 +2,18 @@
 set -ev
 
 mkdir build && cd build
+# Not using CXXFLAGS in order to avoid affecting dependencies
 cmake .. \
+    -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
     -DBUILD_TESTS=ON \
     -DCMAKE_BUILD_TYPE=Debug
 make -j install
-CORRADE_TEST_COLOR=ON ctest -V
+ASAN_OPTIONS="color=always" LSAN_OPTIONS="color=always" CORRADE_TEST_COLOR=ON ctest -V
 cd ..
 
 # Examples
 mkdir build-examples && cd build-examples
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$HOME/deps ../src/examples
+cmake -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$HOME/deps ../src/examples
 make -j
