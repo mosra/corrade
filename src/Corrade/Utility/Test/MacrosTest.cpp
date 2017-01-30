518f413508fb59e05bdaab46462e1c670fe75f54
Vladimír Vondruš <mosra@centrum.cz>
2017-01-30 17:59:34 +0100

    Utility: properly test all CORRADE_DEPRECATED_*() warnings.

--- src/Corrade/Utility/Test/MacrosTest.cpp
+++ src/Corrade/Utility/Test/MacrosTest.cpp
@@ -31,16 +31,58 @@ struct MacrosTest: TestSuite::Tester {
     explicit MacrosTest();
 
     void alignAs();
+    void deprecated();
 };
 
 MacrosTest::MacrosTest() {
-    addTests({&MacrosTest::alignAs});
+    addTests({&MacrosTest::alignAs,
+              &MacrosTest::deprecated});
 }
 
 void MacrosTest::alignAs() {
     CORRADE_ALIGNAS(16) const char aligned[3]{};
     CORRADE_COMPARE(reinterpret_cast<std::uintptr_t>(aligned) % 16, 0);
 }
+
+namespace {
+
+/* Declarations on their own shouldn't produce any compiler diagnostics */
+CORRADE_DEPRECATED("use function() instead") int deprecatedFunction() { return 1; }
+struct CORRADE_DEPRECATED("use Struct instead") DeprecatedStruct { enum: int { Value = 1 }; };
+struct Struct { enum: int { Value = 1 }; };
+using DeprecatedAlias CORRADE_DEPRECATED_ALIAS("use Struct instead") = Struct;
+enum class CORRADE_DEPRECATED_ENUM("use Enum instead") DeprecatedEnum { Value = 1 };
+enum class Foo { DeprecatedEnumValue CORRADE_DEPRECATED_ENUM("use Foo::Value instead") = 1 };
+/* Not sure how to test CORRADE_DEPRECATED_FILE() though */
+
+}
+
+/* Uncomment to test deprecation warnings */
+// #define ENABLE_DEPRECATION_WARNINGS
+
+#ifndef ENABLE_DEPRECATION_WARNINGS
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable: 4996)
+#endif
+#endif
+void MacrosTest::deprecated() {
+    CORRADE_VERIFY(deprecatedFunction());
+    CORRADE_VERIFY(DeprecatedStruct::Value);
+    CORRADE_VERIFY(DeprecatedAlias::Value);
+    CORRADE_VERIFY(int(DeprecatedEnum::Value));
+    CORRADE_VERIFY(int(Foo::DeprecatedEnumValue));
+}
+#ifndef ENABLE_DEPRECATION_WARNINGS
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#elif defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+#endif
 
 }}}
 
