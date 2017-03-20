3dce2fd3250fb80d007b249007298997c644462b
Vladimír Vondruš <mosra@centrum.cz>
2017-03-20 12:17:26 +0100

    package/ci: added a build w/o deprecated features.
    In order to catch issues as the one fixed in
    66fe7500644a1ed3b877caf972ab8c8a68d01549.

--- package/ci/travis-desktop.sh
+++ package/ci/travis-desktop.sh
@@ -8,6 +8,7 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCMAKE_INSTALL_RPATH=$HOME/deps/lib \
     -DBUILD_TESTS=ON \
+    -DBUILD_DEPRECATED=$BUILD_DEPRECATED \
     -DCMAKE_BUILD_TYPE=Debug
 make -j install
 ASAN_OPTIONS="color=always" LSAN_OPTIONS="color=always" CORRADE_TEST_COLOR=ON ctest -V
