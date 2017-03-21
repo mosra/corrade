059e7e9c9c23c13c1809b9af1d55949228dd1788
Vladimír Vondruš <mosra@centrum.cz>
2017-03-21 02:56:27 +0100

    package/ci: don't exit if iOS build *doesn't* fail.
    OMG what am I doing.

--- package/ci/travis-ios-simulator.sh
+++ package/ci/travis-ios-simulator.sh
@@ -26,5 +26,5 @@ cmake .. \
     -DBUILD_TESTS=ON \
     -DTESTSUITE_TARGET_XCTEST=ON \
     -G Xcode
-cmake --build . --config Release | xcpretty && exit ${PIPESTATUS[0]}
+set -o pipefail && cmake --build . --config Release | xcpretty
 CORRADE_TEST_COLOR=ON ctest -V -C Release
