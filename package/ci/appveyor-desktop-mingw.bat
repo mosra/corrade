6706a4f1b52082243df38f4c873cd14efefee0c3
Vladimír Vondruš <mosra@centrum.cz>
2018-02-02 18:58:33 +0100

    package/ci: codecov on AppVeyor MinGW build.
    BOY THIS WAS PAINFUL.

--- package/ci/appveyor-desktop-mingw.bat
+++ package/ci/appveyor-desktop-mingw.bat
@@ -5,7 +5,8 @@ set PATH=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw64\bin;%APPVEYOR_BU
 rem Build
 mkdir build && cd build || exit /b
 cmake .. ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_CXX_FLAGS="--coverage" ^
+    -DCMAKE_BUILD_TYPE=Debug ^
     -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/deps ^
     -DBUILD_TESTS=ON ^
     -G Ninja || exit /b
@@ -20,7 +21,13 @@ rem Examples
 cd %APPVEYOR_BUILD_FOLDER% || exit /b
 mkdir build-examples && cd build-examples || exit /b
 cmake ../src/examples ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_BUILD_TYPE=Debug ^
     -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
     -G Ninja || exit /b
 cmake --build . || exit /b
+
+rem Coverage upload
+cd %APPVEYOR_BUILD_FOLDER%/build
+set PATH=C:\msys64\usr\bin;%PATH%
+bash %APPVEYOR_BUILD_FOLDER%\package\ci\appveyor-lcov.sh || exit /b
+codecov -f coverage.info -X gcov
