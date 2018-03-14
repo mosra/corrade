aba62676cfabd88363396ba0ac01fd0c64ada6b1
Vladimír Vondruš <mosra@centrum.cz>
2018-03-14 16:34:06 +0100

    TestSuite: properly propagate environment to ADB shell.
    I remember I was fighting this because it wasn't working on Travis. But
    this way the environment is propagated only to the `cd` command and not
    to the actual test exe. I hope it works there now.

--- src/Corrade/TestSuite/AdbRunner.sh
+++ src/Corrade/TestSuite/AdbRunner.sh
@@ -82,7 +82,7 @@ for file in "$@"; do
 done
 
 # No comment. https://code.google.com/p/android/issues/detail?id=3254
-adb shell $test_env' cd '$remote_tmpdir' && ./'$filename'; echo -n ADB_IS_SHIT:$?' | tee $tmpdir/adb.retval | grep -v ADB_IS_SHIT
+adb shell 'cd '$remote_tmpdir' && '$test_env' ./'$filename'; echo -n ADB_IS_SHIT:$?' | tee $tmpdir/adb.retval | grep -v ADB_IS_SHIT
 returncode=$(grep ADB_IS_SHIT $tmpdir/adb.retval | cut -d':' -f2)
 
 # Clean up after ourselves -- remove the temporary directories both on local
