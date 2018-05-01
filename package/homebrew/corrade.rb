f28c49840aec836d1d8eac35437cd8476ed2559a
Vladimír Vondruš <mosra@centrum.cz>
2018-05-01 20:25:39 +0200

    package/homebrew: provide stable versions as well.
    Wanted to do this in time for 2018.02 but couldn't make it.

--- package/homebrew/corrade.rb
+++ package/homebrew/corrade.rb
@@ -3,6 +3,8 @@
 class Corrade < Formula
   desc "C++11/C++14 multiplatform utility library"
   homepage "http://magnum.graphics/corrade/"
+  url "https://github.com/mosra/corrade/archive/v2018.02.tar.gz"
+  sha256 "6f05160b24dd34cdbd2b30d72972c750f20dd0982dd647780fb55e710626c4cc"
   head "git://github.com/mosra/corrade.git"
 
   depends_on "cmake"
