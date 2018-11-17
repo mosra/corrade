eb99062af99d4e2bd049b2a1bdb5a2887f059cba
Vladimír Vondruš <mosra@centrum.cz>
2018-11-17 22:46:49 +0100

    New Doxygen command for providing third-party component license info.
    Will get used mainly in Magnum, but having it documented mainly here.

---
+++ doc/codingstyle/doxygen-thirdparty.h
@@ -0,0 +1,9 @@
+/**
+@m_class{m-block m-success}
+
+@thirdparty This plugin makes use of the [HarfBuzz](http://harfbuzz.org/)
+    library, licensed under @m_class{m-label m-success} **MIT**
+    ([license text](https://raw.githubusercontent.com/behdad/harfbuzz/master/COPYING),
+    [choosealicense.com](https://choosealicense.com/licenses/mit/)). It
+    requires attribution for public use.
+*/
