09821029679f9e8a21bfd5b1bc040cff72a2c606
Vladimír Vondruš <mosra@centrum.cz>
2018-12-15 13:31:12 +0100

    Utility: minor cleanup in Tweakable test.
    No functional change.

--- src/Corrade/Utility/Test/TweakableTest.cpp
+++ src/Corrade/Utility/Test/TweakableTest.cpp
@@ -56,69 +56,71 @@ struct TweakableTest: TestSuite::Tester {
 };
 
 namespace {
-    constexpr struct {
-        const char* name;
-        const char* data;
-        const char* alias;
-        const char* expectFail;
-    } TweakableAliasData[]{
-        {"usual", "// a comment\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
-        {"right after another define", "// a comment\n#define something foo\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
-        {"right after an empty define", "// a comment\n#define something\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
-        {"first line", "#define _ CORRADE_TWEAKABLE\n", "_", nullptr},
-        {"windows newlines", "#define T3 CORRADE_TWEAKABLE\r\n", "T3", nullptr},
-        {"as a last in the file", "#define t3 CORRADE_TWEAKABLE", "t3", nullptr},
-        {"spaces", "#define \t t \tCORRADE_TWEAKABLE   \n", "t", nullptr},
-        {"spaces before", "   #define _ CORRADE_TWEAKABLE\n", "_", nullptr},
-        {"spaces after #", "#  define _ CORRADE_TWEAKABLE\n", "_",
-            "Spaces after # not supported."},
-        {"junk after", "#define _ CORRADE_TWEAKABLEs", "CORRADE_TWEAKABLE", nullptr},
-        {"junk after #2", "#define _ CORRADE_TWEAKABLE \tabc", "CORRADE_TWEAKABLE", nullptr},
-        {"commented out", "//#define _ CORRADE_TWEAKABLE\n", "CORRADE_TWEAKABLE", nullptr},
-        {"commented out multiline", "/*\n#define _ CORRADE_TWEAKABLE\n*/", "CORRADE_TWEAKABLE",
-            "Multi-line comments are not handled properly."}
-    };
 
-    constexpr struct {
-        const char* name;
-        const char* data;
-        TweakableState(*parser)(Containers::ArrayView<const char>, Containers::StaticArrayView<Implementation::TweakableStorageSize, char>);
-        TweakableState state;
-        const char* error;
-    } ParseErrorData[]{
-        {"unterminated before", "_(    ", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unterminated _( in a.cpp:1\n"},
-        {"unterminated after", "_(3   ", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unterminated _(3    in a.cpp:1\n"},
-        {"garbage after", "_(3(", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unterminated _(3 in a.cpp:1\n"},
-        {"unterminated string", "_( \"", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unterminated string _( \" in a.cpp:1\n"},
-        {"unterminated char", "_(\t'", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unterminated char _(\t' in a.cpp:1\n"},
-        {"wide char", "_(L' ')", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unsupported wide char/string literal _(L in a.cpp:1\n"},
-        {"unicode 1", "_(U' ')", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(U in a.cpp:1\n"},
-        {"unicode 2", "_(u\" \")", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(u in a.cpp:1\n"},
-        {"unicode 3", "_(u8\" \")", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(u in a.cpp:1\n"},
-        {"raw", "_(R\"( )\")", nullptr, TweakableState::Error,
-            "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(R in a.cpp:1\n"},
-        {"char escape error", "_('\\o')",
-            Implementation::TweakableTraits<char>::parse, TweakableState::Error,
-            /** @todo this message will change once escapes *are* implemented */
-            "Utility::TweakableParser: escape sequences in char literals are not implemented, sorry\n"
-            "Utility::Tweakable::update(): error parsing _('\\o') in a.cpp:1\n"},
-        {"different type", "_(42.0f)",
-            Implementation::TweakableTraits<long>::parse, TweakableState::Recompile,
-            "Utility::TweakableParser: 42.0f has an unexpected suffix, expected l\n"
-            "Utility::Tweakable::update(): change of _(42.0f) in a.cpp:1 requested a recompile\n"},
-        {"unexpected line number", "\n_(false)",
-            Implementation::TweakableTraits<bool>::parse, TweakableState::Recompile,
-            "Utility::Tweakable::update(): code changed around _(false) in a.cpp:2, requesting a recompile\n"}
-    };
+constexpr struct {
+    const char* name;
+    const char* data;
+    const char* alias;
+    const char* expectFail;
+} TweakableAliasData[]{
+    {"usual", "// a comment\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
+    {"right after another define", "// a comment\n#define something foo\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
+    {"right after an empty define", "// a comment\n#define something\n#define T3 CORRADE_TWEAKABLE\n", "T3", nullptr},
+    {"first line", "#define _ CORRADE_TWEAKABLE\n", "_", nullptr},
+    {"windows newlines", "#define T3 CORRADE_TWEAKABLE\r\n", "T3", nullptr},
+    {"as a last in the file", "#define t3 CORRADE_TWEAKABLE", "t3", nullptr},
+    {"spaces", "#define \t t \tCORRADE_TWEAKABLE   \n", "t", nullptr},
+    {"spaces before", "   #define _ CORRADE_TWEAKABLE\n", "_", nullptr},
+    {"spaces after #", "#  define _ CORRADE_TWEAKABLE\n", "_",
+        "Spaces after # not supported."},
+    {"junk after", "#define _ CORRADE_TWEAKABLEs", "CORRADE_TWEAKABLE", nullptr},
+    {"junk after #2", "#define _ CORRADE_TWEAKABLE \tabc", "CORRADE_TWEAKABLE", nullptr},
+    {"commented out", "//#define _ CORRADE_TWEAKABLE\n", "CORRADE_TWEAKABLE", nullptr},
+    {"commented out multiline", "/*\n#define _ CORRADE_TWEAKABLE\n*/", "CORRADE_TWEAKABLE",
+        "Multi-line comments are not handled properly."}
+};
+
+constexpr struct {
+    const char* name;
+    const char* data;
+    TweakableState(*parser)(Containers::ArrayView<const char>, Containers::StaticArrayView<Implementation::TweakableStorageSize, char>);
+    TweakableState state;
+    const char* error;
+} ParseErrorData[]{
+    {"unterminated before", "_(    ", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unterminated _( in a.cpp:1\n"},
+    {"unterminated after", "_(3   ", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unterminated _(3    in a.cpp:1\n"},
+    {"garbage after", "_(3(", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unterminated _(3 in a.cpp:1\n"},
+    {"unterminated string", "_( \"", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unterminated string _( \" in a.cpp:1\n"},
+    {"unterminated char", "_(\t'", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unterminated char _(\t' in a.cpp:1\n"},
+    {"wide char", "_(L' ')", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unsupported wide char/string literal _(L in a.cpp:1\n"},
+    {"unicode 1", "_(U' ')", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(U in a.cpp:1\n"},
+    {"unicode 2", "_(u\" \")", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(u in a.cpp:1\n"},
+    {"unicode 3", "_(u8\" \")", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(u in a.cpp:1\n"},
+    {"raw", "_(R\"( )\")", nullptr, TweakableState::Error,
+        "Utility::Tweakable::update(): unsupported unicode/raw char/string literal _(R in a.cpp:1\n"},
+    {"char escape error", "_('\\o')",
+        Implementation::TweakableTraits<char>::parse, TweakableState::Error,
+        /** @todo this message will change once escapes *are* implemented */
+        "Utility::TweakableParser: escape sequences in char literals are not implemented, sorry\n"
+        "Utility::Tweakable::update(): error parsing _('\\o') in a.cpp:1\n"},
+    {"different type", "_(42.0f)",
+        Implementation::TweakableTraits<long>::parse, TweakableState::Recompile,
+        "Utility::TweakableParser: 42.0f has an unexpected suffix, expected l\n"
+        "Utility::Tweakable::update(): change of _(42.0f) in a.cpp:1 requested a recompile\n"},
+    {"unexpected line number", "\n_(false)",
+        Implementation::TweakableTraits<bool>::parse, TweakableState::Recompile,
+        "Utility::Tweakable::update(): code changed around _(false) in a.cpp:2, requesting a recompile\n"}
+};
+
 }
 
 TweakableTest::TweakableTest() {
@@ -154,7 +156,7 @@ void TweakableTest::constructMove() {
 }
 
 void TweakableTest::findTweakableAlias() {
-    auto& data = TweakableAliasData[testCaseInstanceId()];
+    auto&& data = TweakableAliasData[testCaseInstanceId()];
     setTestCaseDescription(data.name);
     {
         CORRADE_EXPECT_FAIL_IF(data.expectFail, String::fromArray(data.expectFail));
@@ -255,7 +257,7 @@ _('\'') // also no parser
 }
 
 void TweakableTest::parseTweakablesError() {
-    auto& data = ParseErrorData[testCaseInstanceId()];
+    auto&& data = ParseErrorData[testCaseInstanceId()];
     setTestCaseDescription(data.name);
     std::set<std::tuple<void(*)(void(*)(), void*), void(*)(), void*>> scopes;
 
