4eb1adc0948052bec95a5f1149a9a57e253563f1
Vladimír Vondruš <mosra@centrum.cz>
2019-01-23 01:37:35 +0100

    acme: oh, Travis doesn't fetch tags.

--- src/acme/test/revision_stats/expected.h
+++ src/acme/test/revision_stats/expected.h
@@ -2,5 +2,5 @@
     This file is generated from version v2018.10.
 */
 
-// This particular file has exactly 343 bytes,
+// This particular file has exactly 315 bytes,
 // while the processed one has 6 lines.
--- src/acme/test/revision_stats/input.h
+++ src/acme/test/revision_stats/input.h
@@ -2,7 +2,7 @@
     This file is generated from version {{revision}}.
 */
 
-#pragma ACME revision * git describe --match 'v2018.*' --abbrev=0
+#pragma ACME revision * echo v2018.10
 #pragma ACME revision acme/test/revision cat input.h | wc -c
 #pragma ACME stats wc wc -l
 
