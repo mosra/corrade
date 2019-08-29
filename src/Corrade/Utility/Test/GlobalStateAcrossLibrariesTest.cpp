21ec22720f66bfe41d46a87b40ee3e0efc35c571
Vladimír Vondruš <mosra@centrum.cz>
2019-08-29 14:40:16 +0200

    PluginManager, Utility: add tests for global state shared across libraries.
    In particular, it becomes duplicated when Corrade is built statically and
    linked to more than one shared library (or separately to a shared library
    and an executable). Fixes following.

---
+++ src/Corrade/Utility/Test/GlobalStateAcrossLibrariesTest.cpp
@@ -0,0 +1,71 @@
+/*
+    This file is part of Corrade.
+
+    Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+                2017, 2018, 2019 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include <sstream>
+
+#include "Corrade/TestSuite/Tester.h"
+#include "Corrade/Utility/Resource.h"
+
+#include "GlobalStateAcrossLibrariesLibrary.h"
+
+namespace Corrade { namespace Utility { namespace Test { namespace {
+
+struct GlobalStateAcrossLibrariesTest: TestSuite::Tester {
+    explicit GlobalStateAcrossLibrariesTest();
+
+    void debug();
+    void resource();
+};
+
+GlobalStateAcrossLibrariesTest::GlobalStateAcrossLibrariesTest() {
+    addTests({&GlobalStateAcrossLibrariesTest::debug,
+              &GlobalStateAcrossLibrariesTest::resource});
+}
+
+void GlobalStateAcrossLibrariesTest::debug() {
+    std::ostringstream out;
+
+    std::ostream* current = Debug::output();
+    CORRADE_COMPARE(debugOutputFromALibrary(), current);
+
+    {
+        Debug redirectOutput{&out};
+        CORRADE_COMPARE(debugOutputFromALibrary(), &out);
+    }
+
+    CORRADE_COMPARE(debugOutputFromALibrary(), current);
+}
+
+void GlobalStateAcrossLibrariesTest::resource() {
+    /* The resource is complied into the the library, but the executable should
+       see it too */
+    CORRADE_VERIFY(libraryHasATestResourceGroup());
+
+    CORRADE_VERIFY(Utility::Resource::hasGroup("test"));
+}
+
+}}}}
+
+CORRADE_TEST_MAIN(Corrade::Utility::Test::GlobalStateAcrossLibrariesTest)
