ad051654620922dda039fb7e6110e7e46248364c
Vladimír Vondruš <mosra@centrum.cz>
2019-08-31 23:58:41 +0200

    Containers: added an (internal) forward list implementation.
    Will get used for allocation-free registration of resources and static
    plugins on startup.

---
+++ src/Corrade/Containers/Implementation/RawForwardList.h
@@ -0,0 +1,86 @@
+#ifndef Corrade_Containers_Implementation_RawForwardList_h
+#define Corrade_Containers_Implementation_RawForwardList_h
+/*
+    This file is part of Corrade.
+
+    Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+                2017, 2018, 2019 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+namespace Corrade { namespace Containers { namespace Implementation {
+
+/*
+    Utilities used by Resource and PluginManager to manage allocation-free
+    resource / plugin registration at app startup.
+
+    The item type is expected to have a `next` member, initialized to
+    `nullptr`. When added to the list, the `next` member is either set to
+    previous list head or to self, to indicate end of the list -- it's not
+    `nullptr` in order to make it easy to check if it's already in the list.
+*/
+
+template<class T> inline void forwardListInsert(T*& list, T& item) {
+    /* If the item is already in a list, do nothing -- handles plugin /
+       resource registration done more than once. */
+    if(item.next) return;
+
+    /* If this is the first item in the list, set its next to `nullptr` to
+       make it possible to distinguish if the item is in a list or not (as
+       it would be `nullptr` otherwise as well. */
+    if(list) item.next = list;
+    else item.next = &item;
+
+    /* The item is the new list head */
+    list = &item;
+}
+
+template<class T> inline void forwardListRemove(T*& list, T& item) {
+    /* If the item is not in a list, do nothing -- handles plugin /
+       resource registration done more than once. */
+    if(!item.next) return;
+
+    /* Special case if the item is first */
+    if(list == &item) {
+        /* ... and if it's last as well */
+        if(item.next == &item) list = nullptr;
+        else list = item.next;
+        item.next = nullptr;
+        return;
+    }
+
+    /* Assuming the item is in the list, this shouldn't cycle */
+    T* prev = list;
+    while(prev->next != &item) prev = prev->next;
+
+    /* Special case if the item is last */
+    if(item.next == &item) prev->next = prev;
+    else prev->next = item.next;
+    item.next = nullptr;
+}
+
+template<class T> inline T* forwardListNext(T& item) {
+    /* List end is denoted by pointer to self, return nullptr in that case */
+    return item.next == &item ? nullptr : item.next;
+}
+
+}}}
+
+#endif
---
+++ src/Corrade/Containers/Test/RawForwardListTest.cpp
@@ -0,0 +1,151 @@
+/*
+    This file is part of Corrade.
+
+    Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+                2017, 2018, 2019 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "Corrade/Containers/Implementation/RawForwardList.h"
+#include "Corrade/TestSuite/Tester.h"
+
+namespace Corrade { namespace Containers { namespace Test { namespace {
+
+struct RawForwardListTest: TestSuite::Tester {
+    explicit RawForwardListTest();
+
+    void insert();
+    void remove();
+    void next();
+};
+
+RawForwardListTest::RawForwardListTest() {
+    addTests({&RawForwardListTest::insert,
+              &RawForwardListTest::remove,
+              &RawForwardListTest::next});
+}
+
+struct Item {
+    Item* next{};
+};
+
+void RawForwardListTest::insert() {
+    Item a, b, c;
+    Item* list{};
+
+    Implementation::forwardListInsert(list, a);
+    CORRADE_COMPARE(list, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    Implementation::forwardListInsert(list, b);
+    CORRADE_COMPARE(list, &b);
+    CORRADE_COMPARE(b.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    Implementation::forwardListInsert(list, c);
+    CORRADE_COMPARE(list, &c);
+    CORRADE_COMPARE(c.next, &b);
+    CORRADE_COMPARE(b.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    /* Inserting existing should be a no-op */
+    Implementation::forwardListInsert(list, b);
+    CORRADE_COMPARE(list, &c);
+    CORRADE_COMPARE(c.next, &b);
+    CORRADE_COMPARE(b.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+}
+
+void RawForwardListTest::remove() {
+    Item a, b, c, d;
+
+    Item* list{};
+    Implementation::forwardListInsert(list, a);
+    Implementation::forwardListInsert(list, b);
+    Implementation::forwardListInsert(list, c);
+    Implementation::forwardListInsert(list, d);
+    CORRADE_COMPARE(list, &d);
+    CORRADE_COMPARE(d.next, &c);
+    CORRADE_COMPARE(c.next, &b);
+    CORRADE_COMPARE(b.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    /* From the middle */
+    Implementation::forwardListRemove(list, b);
+    CORRADE_COMPARE(b.next, nullptr);
+    CORRADE_COMPARE(list, &d);
+    CORRADE_COMPARE(d.next, &c);
+    CORRADE_COMPARE(c.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    /* From the end */
+    Implementation::forwardListRemove(list, a);
+    CORRADE_COMPARE(a.next, nullptr);
+    CORRADE_COMPARE(list, &d);
+    CORRADE_COMPARE(d.next, &c);
+    CORRADE_COMPARE(c.next, &c);
+
+    /* From the beginning */
+    Implementation::forwardListRemove(list, d);
+    CORRADE_COMPARE(d.next, nullptr);
+    CORRADE_COMPARE(list, &c);
+    CORRADE_COMPARE(c.next, &c);
+
+    /* From the beginning and also the end */
+    Implementation::forwardListRemove(list, c);
+    CORRADE_COMPARE(c.next, nullptr);
+    CORRADE_COMPARE(list, nullptr);
+
+    /* Removing an item that isn't there should be a no-op */
+    Implementation::forwardListInsert(list, a);
+    CORRADE_COMPARE(list, &a);
+    CORRADE_COMPARE(a.next, &a);
+    Implementation::forwardListRemove(list, b);
+    CORRADE_COMPARE(list, &a);
+    CORRADE_COMPARE(a.next, &a);
+    CORRADE_COMPARE(b.next, nullptr);
+}
+
+void RawForwardListTest::next() {
+    Item a, b, c;
+
+    Item* list{};
+    Implementation::forwardListInsert(list, a);
+    Implementation::forwardListInsert(list, b);
+    Implementation::forwardListInsert(list, c);
+    CORRADE_COMPARE(list, &c);
+    CORRADE_COMPARE(c.next, &b);
+    CORRADE_COMPARE(b.next, &a);
+    CORRADE_COMPARE(a.next, &a);
+
+    /* Second item */
+    CORRADE_COMPARE(Implementation::forwardListNext(*list), &b);
+    CORRADE_COMPARE(Implementation::forwardListNext(c), &b);
+
+    /* Third / last item */
+    CORRADE_COMPARE(Implementation::forwardListNext(b), &a);
+
+    /* End */
+    CORRADE_COMPARE(Implementation::forwardListNext(a), nullptr);
+}
+
+}}}}
+
+CORRADE_TEST_MAIN(Corrade::Containers::Test::RawForwardListTest)
