b87f7ab1cedaf95651c644e522d2e78167de784f
Vladimír Vondruš <mosra@centrum.cz>
2019-12-11 19:14:50 +0100

    package/ci: enable clang-cl on AppVeyor.

--- package/ci/appveyor-desktop.bat
+++ package/ci/appveyor-desktop.bat
@@ -3,6 +3,12 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" call "C:/Program File
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" %PLATFORM% || exit /b
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps\bin;%PATH%
 
+rem need to explicitly specify a 64-bit target, otherwise CMake+Ninja can't
+rem figure that out -- https://gitlab.kitware.com/cmake/cmake/issues/16259
+rem for TestSuite we need to enable exceptions explicitly with /EH as these are
+rem currently disabled -- https://github.com/catchorg/Catch2/issues/1113
+if "%COMPILER%" == "msvc-clang" set COMPILER_EXTRA=-DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin/clang-cl.exe" -DCMAKE_LINKER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin/lld-link.exe" -DCMAKE_CXX_FLAGS="-m64 /EHsc"
+
 rem Build
 mkdir build && cd build || exit /b
 cmake .. ^
@@ -11,7 +17,7 @@ cmake .. ^
     -DUTILITY_USE_ANSI_COLORS=%ANSI_COLORS% ^
     -DBUILD_TESTS=ON ^
     -DBUILD_STATIC=%BUILD_STATIC% ^
-    -G Ninja || exit /b
+    %COMPILER_EXTRA% -G Ninja || exit /b
 cmake --build . || exit /b
 
 rem Test
@@ -27,5 +33,5 @@ mkdir build-examples && cd build-examples || exit /b
 cmake ../src/examples ^
     -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
     -DCMAKE_PREFIX_PATH=%APPVEYOR_BUILD_FOLDER%/deps ^
-    -G Ninja || exit /b
+    %COMPILER_EXTRA% -G Ninja || exit /b
 cmake --build . || exit /b
