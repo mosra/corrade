44915473ffccb268a20f3b241c043b28e66a944d
Vladimír Vondruš <mosra@centrum.cz>
2019-12-12 11:45:14 +0100

    doc: fix an error on clang-cl.
    Interesting that it complains only there.

--- doc/snippets/Utility.cpp
+++ doc/snippets/Utility.cpp
@@ -670,7 +670,7 @@ int foo(int a, CORRADE_UNUSED int b) {
 CORRADE_ALIGNAS(4) char data[16]; // so it can be read as 32-bit integers
 /* [CORRADE_ALIGNAS] */
 
-void exit42();
+CORRADE_NORETURN void exit42();
 /* [CORRADE_NORETURN] */
 CORRADE_NORETURN void exit42() { std::exit(42); }
 /* [CORRADE_NORETURN] */
