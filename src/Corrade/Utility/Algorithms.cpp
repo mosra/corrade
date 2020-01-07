a5562ce0e66c71e99c7d2d0f2b6b57768a3977d9
Vladimír Vondruš <mosra@centrum.cz>
2020-01-07 17:37:57 +0100

    Utility: delegate 1D/2D/3D copy into the 4D one.
    THe overhead doesn't seem to be that big, and it's making the
    maintenance easier. Might revisit later.

--- src/Corrade/Utility/Algorithms.cpp
+++ src/Corrade/Utility/Algorithms.cpp
@@ -29,6 +29,16 @@
 
 namespace Corrade { namespace Utility {
 
+/* I might be going a bit overboard with the avoidance of inline function calls
+   in Debug -- should revisit with a clearer mind later.
+
+   To avoid maintenance hell, the 1D/2D/3D variants populate a 4D view with the
+   first dimensions having a size of 1, thus "drilling down" directly to the
+   leaf branches of the 4D variant. The overhead isn't too big.
+
+   OTOH the ArrayView overhead can delegate directly to a memcpy, so for code
+   size savings it shouldn't be handled in the complex stride-aware code. */
+
 void copy(const Containers::ArrayView<const void>& src, const Containers::ArrayView<void>& dst) {
     const std::size_t srcSize = src.size();
     const std::size_t dstSize = dst.size();
@@ -44,17 +54,18 @@ void copy(const Containers::StridedArrayView1D<const char>& src, const Container
     CORRADE_ASSERT(srcSize == dstSize,
         "Utility::Algorithms::copy(): sizes" << srcSize << "and" << dstSize << "don't match", );
 
-    auto* const srcPtr = static_cast<const char*>(src.data());
-    auto* const dstPtr = static_cast<char*>(dst.data());
-
-    if(src.isContiguous() && dst.isContiguous())
-        std::memcpy(dstPtr, srcPtr, srcSize);
-    else {
-        const std::ptrdiff_t srcStride = src.stride();
-        const std::ptrdiff_t dstStride = dst.stride();
-        for(std::size_t i = 0; i != srcSize; ++i)
-            *(dstPtr + i*dstStride) = *(srcPtr + i*srcStride);
-    }
+    const std::ptrdiff_t srcStride = src.stride();
+    const std::ptrdiff_t dstStride = dst.stride();
+    /* Using ~std::size_t{} for arrayview size as a shortcut -- there it's just
+       for the size assert anyway */
+    return copy(Containers::StridedArrayView4D<const char>{
+            {static_cast<const char*>(src.data()), ~std::size_t{}},
+            {1, 1, 1, srcSize},
+            {srcStride, srcStride, srcStride, srcStride}},
+        Containers::StridedArrayView4D<char>{
+            {static_cast<char*>(dst.data()), ~std::size_t{}},
+            {1, 1, 1, dstSize},
+            {dstStride, dstStride, dstStride, dstStride}});
 }
 
 void copy(const Containers::StridedArrayView2D<const char>& src, const Containers::StridedArrayView2D<char>& dst) {
@@ -63,33 +74,20 @@ void copy(const Containers::StridedArrayView2D<const char>& src, const Container
     CORRADE_ASSERT(srcSize_ == dstSize_,
         "Utility::Algorithms::copy(): sizes" << srcSize_ << "and" << dstSize_ << "don't match", );
 
-    const std::size_t* const size = srcSize_.begin();
-    auto* const srcPtr = static_cast<const char*>(src.data());
-    auto* const dstPtr = static_cast<char*>(dst.data());
-
-    if(src.isContiguous() && dst.isContiguous())
-        std::memcpy(dstPtr, srcPtr, size[0]*size[1]);
-    else {
-        const Containers::StridedDimensions<2, std::ptrdiff_t> srcStride_ = src.stride();
-        const Containers::StridedDimensions<2, std::ptrdiff_t> dstStride_ = dst.stride();
-        const std::ptrdiff_t* const srcStride = srcStride_.begin();
-        const std::ptrdiff_t* const dstStride = dstStride_.begin();
-
-        if(src.isContiguous<1>() && dst.isContiguous<1>()) {
-            for(std::size_t i = 0; i != size[0]; ++i)
-                std::memcpy(dstPtr + i*dstStride[0],
-                            srcPtr + i*srcStride[0], size[1]);
-
-        } else {
-            for(std::size_t i0 = 0; i0 != size[0]; ++i0) {
-                const char* srcPtr0 = srcPtr + i0*srcStride[0];
-                char* dstPtr0 = dstPtr + i0*dstStride[0];
-                for(std::size_t i1 = 0; i1 != size[1]; ++i1)
-                    *(dstPtr0 + i1*dstStride[1]) =
-                        *(srcPtr0 + i1*srcStride[1]);
-            }
-        }
-    }
+    const std::size_t* const srcSize = srcSize_.begin();
+    const std::size_t* const dstSize = dstSize_.begin();
+    const std::ptrdiff_t* const srcStride = src.stride().begin();
+    const std::ptrdiff_t* const dstStride = dst.stride().begin();
+    /* Using ~std::size_t{} for arrayview size as a shortcut -- there it's just
+       for the size assert anyway */
+    return copy(Containers::StridedArrayView4D<const char>{
+            {static_cast<const char*>(src.data()), ~std::size_t{}},
+            {1, 1, srcSize[0], srcSize[1]},
+            {srcStride[0], srcStride[0], srcStride[0], srcStride[1]}},
+        Containers::StridedArrayView4D<char>{
+            {static_cast<char*>(dst.data()), ~std::size_t{}},
+            {1, 1, dstSize[0], dstSize[1]},
+            {dstStride[0], dstStride[0], dstStride[0], dstStride[1]}});
 }
 
 void copy(const Containers::StridedArrayView3D<const char>& src, const Containers::StridedArrayView3D<char>& dst) {
@@ -98,47 +96,20 @@ void copy(const Containers::StridedArrayView3D<const char>& src, const Container
     CORRADE_ASSERT(srcSize_ == dstSize_,
         "Utility::Algorithms::copy(): sizes" << srcSize_ << "and" << dstSize_ << "don't match", );
 
-    const std::size_t* const size = srcSize_.begin();
-    auto* const srcPtr = static_cast<const char*>(src.data());
-    auto* const dstPtr = static_cast<char*>(dst.data());
-
-    if(src.isContiguous() && dst.isContiguous())
-        std::memcpy(dstPtr, srcPtr, size[0]*size[1]*size[2]);
-    else {
-        const Containers::StridedDimensions<3, std::ptrdiff_t> srcStride_ = src.stride();
-        const Containers::StridedDimensions<3, std::ptrdiff_t> dstStride_ = dst.stride();
-        const std::ptrdiff_t* const srcStride = srcStride_.begin();
-        const std::ptrdiff_t* const dstStride = dstStride_.begin();
-
-        if(src.isContiguous<1>() && dst.isContiguous<1>()) {
-            const std::size_t size12 = size[1]*size[2];
-            for(std::size_t i = 0; i != size[0]; ++i)
-                std::memcpy(dstPtr + i*dstStride[0],
-                            srcPtr + i*srcStride[0], size12);
-        } else {
-            if(src.isContiguous<2>() && dst.isContiguous<2>()) {
-                for(std::size_t i0 = 0; i0 != size[0]; ++i0) {
-                    const char* srcPtr0 = srcPtr + i0*srcStride[0];
-                    char* dstPtr0 = dstPtr + i0*dstStride[0];
-                    for(std::size_t i1 = 0; i1 != size[1]; ++i1)
-                        std::memcpy(dstPtr0 + i1*dstStride[1],
-                                    srcPtr0 + i1*srcStride[1], size[2]);
-                }
-            } else {
-                for(std::size_t i0 = 0; i0 != size[0]; ++i0) {
-                    const char* srcPtr0 = srcPtr + i0*srcStride[0];
-                    char* dstPtr0 = dstPtr + i0*dstStride[0];
-                    for(std::size_t i1 = 0; i1 != size[1]; ++i1) {
-                        const char* srcPtr1 = srcPtr0 + i1*srcStride[1];
-                        char* dstPtr1 = dstPtr0 + i1*dstStride[1];
-                        for(std::size_t i2 = 0; i2 != size[2]; ++i2)
-                            *(dstPtr1 + i2*dstStride[2]) =
-                                *(srcPtr1 + i2*srcStride[2]);
-                    }
-                }
-            }
-        }
-    }
+    const std::size_t* const srcSize = srcSize_.begin();
+    const std::size_t* const dstSize = dstSize_.begin();
+    const std::ptrdiff_t* const srcStride = src.stride().begin();
+    const std::ptrdiff_t* const dstStride = dst.stride().begin();
+    /* Using ~std::size_t{} for arrayview size as a shortcut -- there it's just
+       for the size assert anyway */
+    return copy(Containers::StridedArrayView4D<const char>{
+            {static_cast<const char*>(src.data()), ~std::size_t{}},
+            {1, srcSize[0], srcSize[1], srcSize[2]},
+            {srcStride[0], srcStride[0], srcStride[1], srcStride[2]}},
+        Containers::StridedArrayView4D<char>{
+            {static_cast<char*>(dst.data()), ~std::size_t{}},
+            {1, dstSize[0], dstSize[1], dstSize[2]},
+            {dstStride[0], dstStride[0], dstStride[1], dstStride[2]}});
 }
 
 void copy(const Containers::StridedArrayView4D<const char>& src, const Containers::StridedArrayView4D<char>& dst) {
