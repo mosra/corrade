cac26265e45fb15e20e20a1902071ccddb4b0729
Burak Canik <canik.burak@gmail.com>
2020-05-19 06:22:10 +0300

    Add missing unsigned int cast.

--- src/Corrade/Utility/ConfigurationGroup.h
+++ src/Corrade/Utility/ConfigurationGroup.h
@@ -128,7 +128,7 @@ class CORRADE_UTILITY_EXPORT ConfigurationGroup {
          *
          * @see @ref hasGroups(), @ref valueCount()
          */
-        unsigned int groupCount() const { return _groups.size(); }
+        unsigned int groupCount() const { return unsigned int(_groups.size()); }
 
         /**
          * @brief Whether given group exists
