7e0c03d71d7d6b9647a1d20f2802b30bb431329e
Vladimír Vondruš <mosra@centrum.cz>
2020-06-01 17:53:42 +0200

    Add a version.h header containing the full version information.
    Currently not really documented in Doxygen because ugh I have no idea
    how to make that work.

---
+++ src/Corrade/version.h.cmake
@@ -0,0 +1,38 @@
+#ifndef Corrade_version_h
+#define Corrade_version_h
+/*
+    This file is part of Corrade.
+
+    Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+                2017, 2018, 2019, 2020 Vladimír Vondruš <mosra@centrum.cz>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+/* Note: this header is deliberately not included from anywhere except
+   VersionTest to avoid triggering a full rebuild every time the Git commit
+   hash changes. */
+
+#define CORRADE_VERSION_YEAR ${CORRADE_VERSION_YEAR}
+#define CORRADE_VERSION_MONTH ${CORRADE_VERSION_MONTH}
+#cmakedefine CORRADE_VERSION_COMMIT ${CORRADE_VERSION_COMMIT}
+#cmakedefine CORRADE_VERSION_HASH 0x${CORRADE_VERSION_HASH}
+#cmakedefine CORRADE_VERSION_STRING "${CORRADE_VERSION_STRING}"
+
+#endif
