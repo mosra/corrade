6d66c66d48622365aad7b38d89e94db2f668ca27
Vladimír Vondruš <mosra@centrum.cz>
2020-10-06 13:57:16 +0200

    Containers: include StringView from String.
    It's needed for comparison operators, otherwise if we'd include just
    String, a == b would fail to compile.

--- src/Corrade/Containers/String.h
+++ src/Corrade/Containers/String.h
@@ -36,6 +36,8 @@
 #include <utility>
 
 #include "Corrade/Containers/Containers.h"
+#include "Corrade/Containers/StringView.h" /* needs to be included for
+                                              comparison operators */
 #include "Corrade/Utility/Utility.h"
 #include "Corrade/Utility/visibility.h"
 
