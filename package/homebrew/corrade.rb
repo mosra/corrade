83c804898a7037e3d14d77b951261e1e0273195d
Vladimír Vondruš <mosra@centrum.cz>
2020-10-16 19:36:08 +0200

    package/homebrew: update to what's actually recent.

--- package/homebrew/corrade.rb
+++ package/homebrew/corrade.rb
@@ -1,9 +1,9 @@
 class Corrade < Formula
   desc "C++11/C++14 multiplatform utility library"
   homepage "https://magnum.graphics/corrade/"
-  url "https://github.com/mosra/corrade/archive/v2019.10.tar.gz"
-  # wget https://github.com/mosra/corrade/archive/v2019.10.tar.gz -O - | sha256sum
-  sha256 "19dbf3c0b28a06a7017d627ee7b84c23b994c469198c1134a8aeba9cfbff7ec3"
+  url "https://github.com/mosra/corrade/archive/v2020.06.tar.gz"
+  # wget https://github.com/mosra/corrade/archive/v2020.06.tar.gz -O - | sha256sum
+  sha256 "2a62492ccc717422b72f2596a3e1a6a105b9574aa9467917f12d19ef3aab1341"
   head "git://github.com/mosra/corrade.git"
 
   depends_on "cmake"
@@ -11,7 +11,10 @@ class Corrade < Formula
   def install
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", ".."
+      system "cmake",
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+        ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
