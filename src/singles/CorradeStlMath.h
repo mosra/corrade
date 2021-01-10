2e238eacd439c3d528568844eb1625372acbe379
Vladimír Vondruš <mosra@centrum.cz>
2021-01-10 15:52:53 +0100

    Update copyright year.

--- src/singles/CorradeStlMath.h
+++ src/singles/CorradeStlMath.h
@@ -30,7 +30,8 @@
 #define Corrade_Utility_StlMath_h
 /*
     Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-                2017, 2018, 2019, 2020 Vladimír Vondruš <mosra@centrum.cz>
+                2017, 2018, 2019, 2020, 2021
+              Vladimír Vondruš <mosra@centrum.cz>
 */
 
 #ifdef _GLIBCXX_USE_STD_SPEC_FUNCS
