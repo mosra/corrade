7563edd50f5c3ea74445541132e5998ca70afd25
Vladimír Vondruš <mosra@centrum.cz>
2022-02-09 13:01:35 +0100

    package/ci: add an x86 Android build on CircleCI.
    This looks way too good to be true. A bit sad that it's not ARM, but I
    can easily add a dedicated native ARM build later, without the overhead
    of Android emulation.

---
+++ package/ci/android-x86.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -ev
+
+# Build native corrade-rc
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_INTERCONNECT=OFF \
+    -DWITH_PLUGINMANAGER=OFF \
+    -DWITH_TESTSUITE=OFF \
+    -DWITH_UTILITY=OFF \
+    -G Ninja
+ninja install
+cd ..
+
+# Crosscompile
+mkdir build-android-x86 && cd build-android-x86
+cmake .. \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_SYSTEM_VERSION=29 \
+    -DCMAKE_ANDROID_ARCH_ABI=x86 \
+    -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang \
+    -DCMAKE_ANDROID_STL_TYPE=c++_static \
+    -DCMAKE_INSTALL_PREFIX=$HOME/deps \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
+    -DBUILD_TESTS=ON \
+    -G Ninja
+ninja
+
+# Wait for emulator to start (done in parallel to build) and run tests
+circle-android wait-for-boot
+CORRADE_TEST_COLOR=ON ctest -V
+
+# Test install, after running the tests as for them it shouldn't be needed
+ninja install
