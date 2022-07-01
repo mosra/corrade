8071415b5236550fbe16589cbec3700e721eec8a
Vladimír Vondruš <mosra@centrum.cz>
2022-07-01 19:59:52 +0200

    package/ci: build in parallel also on iOS.
    UGH stupid defaults. STUPID.

--- package/ci/ios-simulator.sh
+++ package/ci/ios-simulator.sh
@@ -28,8 +28,8 @@ cmake .. \
     -DCORRADE_BUILD_TESTS=ON \
     -DCORRADE_TESTSUITE_TARGET_XCTEST=ON \
     -G Xcode
-set -o pipefail && cmake --build . --config Release | xcbeautify
+set -o pipefail && cmake --build . --config Release -j$XCODE_JOBS | xcbeautify
 CORRADE_TEST_COLOR=ON ctest -V -C Release
 
 # Test install, after running the tests as for them it shouldn't be needed
-set -o pipefail && cmake --build . --config Release --target install | xcbeautify
+set -o pipefail && cmake --build . --config Release --target install -j$XCODE_JOBS | xcbeautify
