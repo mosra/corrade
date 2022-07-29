d15c2ea0a2ae509b1abc1c8169d37110853ce32c
Vladimír Vondruš <mosra@centrum.cz>
2022-07-29 19:12:34 +0200

    modules: retrieve Node.js version in FindNodeJs.

--- modules/FindNodeJs.cmake
+++ modules/FindNodeJs.cmake
@@ -6,6 +6,7 @@
 #
 #  NodeJs_FOUND         - True if Node.js executable is found
 #  NodeJs::NodeJs       - Node.js executable imported target
+#  NodeJs_VERSION       - Version string reported by ``node --version``
 #
 
 #
@@ -37,8 +38,21 @@
 find_program(NODEJS_EXECUTABLE node)
 mark_as_advanced(NODEJS_EXECUTABLE)
 
+if(NODEJS_EXECUTABLE)
+    execute_process(COMMAND ${NODEJS_EXECUTABLE} --version
+        OUTPUT_VARIABLE NodeJs_VERSION
+        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NodeJs_VERSION MATCHES "^v[0-9]")
+        string(SUBSTRING ${NodeJs_VERSION} 1 -1 NodeJs_VERSION)
+    else()
+        unset(NodeJs_VERSION)
+    endif()
+endif()
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args("NodeJs" DEFAULT_MSG NODEJS_EXECUTABLE)
+find_package_handle_standard_args(NodeJs
+    REQUIRED_VARS NODEJS_EXECUTABLE
+    VERSION_VAR NodeJs_VERSION)
 
 if(NOT TARGET NodeJs::NodeJs)
     add_executable(NodeJs::NodeJs IMPORTED)
