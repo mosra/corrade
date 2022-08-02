9d5808fd11cd3838b37cfe7308b4d3a42206849a
Vladimír Vondruš <mosra@centrum.cz>
2022-08-02 12:33:49 +0200

    singles: use shorter version hash.
    With now over 1000 commits since last tag and >1k LOC for the input
    file, the version line went beyond 80 chars. Now it doesn't anymore.

--- src/singles/base.h
+++ src/singles/base.h
@@ -50,6 +50,6 @@
 /* Make it possible to include Assert.h multiple times */
 #pragma ACME disable Corrade_Utility_Assert_h
 
-#pragma ACME revision * echo "$(git describe --long --match 'v*') ($(date -d @$(git log -1 --format=%at) +%Y-%m-%d))"
+#pragma ACME revision * echo "$(git describe --long --match 'v*' --abbrev=4) ($(date -d @$(git log -1 --format=%at) +%Y-%m-%d))"
 #pragma ACME stats loc wc -l
 #pragma ACME stats preprocessed g++ -std=c++11 -P -E -x c++ - | wc -l
