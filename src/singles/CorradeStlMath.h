c74508310949b3bfbf05ef74207d3adcbdb10c79
Vladimír Vondruš <mosra@centrum.cz>
2023-06-07 12:59:26 +0200

    It's 2023 already!

--- src/singles/CorradeStlMath.h
+++ src/singles/CorradeStlMath.h
@@ -30,7 +30,7 @@
 #define Corrade_Utility_StlMath_h
 /*
     Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-                2017, 2018, 2019, 2020, 2021, 2022
+                2017, 2018, 2019, 2020, 2021, 2022, 2023
               Vladimír Vondruš <mosra@centrum.cz>
 */
 
