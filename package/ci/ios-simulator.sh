d46c533ef72511dc37181f3962fbd58ad2d6840d
Vladimír Vondruš <mosra@centrum.cz>
2023-08-14 21:38:01 +0200

    [wip]

--- package/ci/ios-simulator.sh
+++ package/ci/ios-simulator.sh
@@ -27,8 +27,15 @@ cmake .. \
     -DCORRADE_BUILD_STATIC=ON \
     -DCORRADE_BUILD_TESTS=ON \
     -DCORRADE_TESTSUITE_TARGET_XCTEST=ON \
+    -DCORRADE_TESTSUITE_XCTEST_DESTINATION="platform=iOS Simulator,name=iPhone 8" \
     -G Xcode
 set -o pipefail && cmake --build . --config Release -j$XCODE_JOBS -- -sdk iphonesimulator | xcbeautify
+
+# Boot a simulator before running tests. It's not required, but makes the tests
+# run significantly faster (apparently because it then doesn't have to boot and
+# shutdown a simulator every time?? ugh)
+xcrun simctl boot "iPhone 8"
+
 CORRADE_TEST_COLOR=ON ctest -V -C Release
 
 # Test install, after running the tests as for them it shouldn't be needed
