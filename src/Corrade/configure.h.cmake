803ae2624db66e4efa7555daa38f1b5aabd0037b
Vladimír Vondruš <mosra@centrum.cz>
2023-12-10 17:09:02 +0100

    Fix CORRADE_TARGET_SIND128 detection on Emscripten 3.1.23+.
    The detection used __EMSCRIPTEN_major__ etc. to exclude Emscripten
    older than 2.0.18 because it didn't have the actual intrinsics in Clang.
    However, on Emscripten 3.1.23+ these are no longer implicitly defined,
    which caused the detection to behave as if an old Emscripten was used,
    never actually defining CORRADE_TARGET_SIMD128.
    
    To fix that, <emscripten/version.h> has to be unconditionally included
    by configure.h. which is a bit nasty. It'll get cleaned up again once
    support for Emscripten pre-2.0.18 is dropped.

--- src/Corrade/configure.h.cmake
+++ src/Corrade/configure.h.cmake
@@ -363,6 +363,16 @@
    well :(
     https://github.com/emscripten-core/emscripten/commit/deab7783df407b260f46352ffad2a77ca8fb0a4c */
 #elif defined(CORRADE_TARGET_WASM)
+/* The __EMSCRIPTEN_major__ etc macros used to be passed implicitly, version
+   3.1.4 moved them to a version header and version 3.1.23 dropped the
+   backwards compatibility. To work consistently on all versions, including the
+   header only if the version macros aren't present.
+   https://github.com/emscripten-core/emscripten/commit/f99af02045357d3d8b12e63793cef36dfde4530a
+   https://github.com/emscripten-core/emscripten/commit/f76ddc702e4956aeedb658c49790cc352f892e4c */
+/** @todo remove the include once we no longer need to check for 2.0.18 */
+#if defined(CORRADE_TARGET_EMSCRIPTEN) && !defined(__EMSCRIPTEN_major__)
+#include <emscripten/version.h>
+#endif
 #if defined(__wasm_simd128__) && __clang_major__ >= 13 && __EMSCRIPTEN_major__*10000 + __EMSCRIPTEN_minor__*100 + __EMSCRIPTEN_tiny__ >= 20018
 #define CORRADE_TARGET_SIMD128
 #endif
