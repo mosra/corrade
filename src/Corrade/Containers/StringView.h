52e496ab3fa418fdf19ed85d26f7f4117f7fb469
Vladimír Vondruš <mosra@centrum.cz>
2024-06-06 16:28:12 +0200

    Ignore Clang's -Wdeprecated-literal-operator until we can drop GCC 4.8.

--- src/Corrade/Containers/StringView.h
+++ src/Corrade/Containers/StringView.h
@@ -1448,6 +1448,17 @@ namespace Literals {
     #endif
     namespace StringLiterals {
 
+/* According to https://wg21.link/CWG2521, space between "" and literal name is
+   deprecated because _Uppercase or __double names could be treated as reserved
+   depending on whether the space was present or not, and whitespace is not
+   load-bearing in any other contexts. Clang 17+ adds an off-by-default warning
+   for this; GCC 4.8 however *requires* the space there, so until GCC 4.8
+   support is dropped, we suppress this warning instead of removing the
+   space. */
+#if defined(CORRADE_TARGET_CLANG) && __clang_major__ >= 17
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-literal-operator"
+#endif
 /** @relatesalso Corrade::Containers::BasicStringView
 @brief String view literal
 @m_since_latest
@@ -1461,6 +1472,9 @@ constexpr StringView operator"" _s(const char* data, std::size_t size) {
     /* Using plain bit ops instead of EnumSet to speed up debug builds */
     return StringView{data, size, StringViewFlag(std::size_t(StringViewFlag::Global)|std::size_t(StringViewFlag::NullTerminated))};
 }
+#if defined(CORRADE_TARGET_CLANG) && __clang_major__ >= 17
+#pragma clang diagnostic pop
+#endif
 
 }}
 
