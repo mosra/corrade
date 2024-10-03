eb58a1eb89cfedf0be66c9eb9e2bc70454aac025
Vladimír Vondruš <mosra@centrum.cz>
2024-10-03 23:14:50 +0200

    package/ci: build on Emscripten with emulated corrade-rc.
    And build the examples there as well, to directly verify that the Find
    module works correctly, and not only later when it gets copied to other
    repos.

--- package/ci/emscripten.sh
+++ package/ci/emscripten.sh
@@ -3,23 +3,9 @@ set -ev
 
 git submodule update --init
 
-# Build native corrade-rc
-mkdir build && cd build || exit /b
-cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$HOME/deps-native \
-    -DCORRADE_WITH_INTERCONNECT=OFF \
-    -DCORRADE_WITH_PLUGINMANAGER=OFF \
-    -DCORRADE_WITH_TESTSUITE=OFF \
-    -DCORRADE_WITH_UTILITY=OFF \
-    -G Ninja
-ninja install
-cd ..
-
 # Crosscompile
 mkdir build-emscripten && cd build-emscripten
 cmake .. \
-    -DCORRADE_RC_EXECUTABLE=$HOME/deps-native/bin/corrade-rc \
     -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten-wasm.cmake" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
@@ -34,3 +20,17 @@ CORRADE_TEST_COLOR=ON ctest -V
 
 # Test install, after running the tests as for them it shouldn't be needed
 ninja install
+
+cd ..
+
+# Examples. This is, among other things, verifying sanity of FindCorrade.cmake
+# especially when it comes to finding the right corrade-rc.
+mkdir build-examples && cd build-examples
+cmake ../src/examples \
+    -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten-wasm.cmake" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O1" \
+    -DCMAKE_FIND_ROOT_PATH=$HOME/deps \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="-O1" \
+    -G Ninja
+ninja
