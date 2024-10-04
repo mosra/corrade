a27d511d6499eb87c30182fe775a3739d11483e2
Vladimír Vondruš <mosra@centrum.cz>
2024-10-04 16:51:20 +0200

    It's October already?!

--- src/Corrade/Utility/Arguments.cpp
+++ src/Corrade/Utility/Arguments.cpp
@@ -2,7 +2,7 @@
     This file is part of Corrade.
 
     Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-                2017, 2018, 2019, 2020, 2021, 2022, 2023
+                2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
               Vladimír Vondruš <mosra@centrum.cz>
     Copyright © 2020 Pablo Escobar <mail@rvrs.in>
     Copyright © 2023 barracuda156 <vital.had@gmail.com>
