8d23e7fc9ad2f135149bc5e207d71da3968020b0
Igal Alkon <igal.alkon@gmail.com>
2024-10-06 17:38:00 +0300

    rpm build tree created by script, readme updated.

--- package/rpm/README.md
+++ package/rpm/README.md
@@ -1,11 +1,6 @@
 # Corrade RPM Package
 
-You will need to install the following dependencies for build rpms:
+You will need to install the following dependencies for build rpms (Fedora Linux):
 ```
 sudo dnf install fedora-packager rpmdevtools gcc
-```
-
-Create user directory tree building RPMs:
-```
-mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 ```
--- package/rpm/build.sh
+++ package/rpm/build.sh
@@ -4,6 +4,9 @@
 version_hash=$(git describe --match "v*" | sed 's/^v//' | sed 's/-/./g')
 echo "** repository hash: ${version_hash} ..."
 
+# create dir tree for rpmbuild in user dir
+rpmdev-setuptree
+
 # archive reository
 (cd ../.. && git archive --format=tar.gz --prefix=corrade-${version_hash}/ -o ~/rpmbuild/SOURCES/corrade-${version_hash}.tar.gz HEAD)
 echo "** created archive: ~/rpmbuild/SOURCES/corrade-${version_hash}.tar.gz"
@@ -11,13 +14,11 @@ sleep 2
 
 # replace spec version
 spec_file="corrade.spec"
-cp ${spec_file} ${spec_file}.build
-sed -i "s/Version:.\+/Version: ${version_hash}/g" ${spec_file}.build
+sed -i "s/Version:.\+/Version: ${version_hash}/g" ${spec_file}
 echo "** building package version: ${version_hash}"
 
 # check dependencies
 sudo dnf builddep -y ${spec_file}
 
 # build package
-rpmbuild --define "debug_package %{nil}" --clean -ba ${spec_file}.build
-rm ${spec_file}.build
+rpmbuild --define "debug_package %{nil}" --clean -ba ${spec_file}
