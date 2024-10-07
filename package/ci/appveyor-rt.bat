ed451d54ed4c7a20d0c4c5b6bc87ede089152b20
Vladimír Vondruš <mosra@centrum.cz>
2024-10-08 01:19:50 +0200

    package/ci: clarify the testing situation on UWP, cleanup.

--- package/ci/appveyor-rt.bat
+++ package/ci/appveyor-rt.bat
@@ -1,6 +1,5 @@
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 || exit /b
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
-if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" set GENERATOR=Visual Studio 14 2015
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" set GENERATOR=Visual Studio 15 2017
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps-native\bin;%PATH%
 
@@ -17,7 +16,8 @@ cmake .. ^
 cmake --build . --target install || exit /b
 cd .. || exit /b
 
-rem Crosscompile
+rem Crosscompile. No tests because they takes ages to build, each executable is
+rem a msix file, and they can't be reasonably run either. F this platform.
 mkdir build-rt && cd build-rt || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -28,5 +28,5 @@ cmake .. ^
     -G "%GENERATOR%" || exit /b
 cmake --build . --config Release -- /m /v:m  || exit /b
 
-rem Test install, after running the tests as for them it shouldn't be needed
+rem Test install
 cmake --build . --config Release --target install || exit /b
