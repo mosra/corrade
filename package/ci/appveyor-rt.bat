37d1d05d49f20a04c88af5eefab6dead502db0b9
Vladimír Vondruš <mosra@centrum.cz>
2024-10-08 12:50:03 +0200

    package/ci: guard against regressions with corrade-rc on UWP.
    If it gets cross-compiled again by accident, the job will get stuck on
    that damn dialog box, immediately notifying me that I fucked up.

--- package/ci/appveyor-rt.bat
+++ package/ci/appveyor-rt.bat
@@ -1,6 +1,11 @@
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat" x64 || exit /b
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x64 || exit /b
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" set GENERATOR=Visual Studio 15 2017
+rem This is what should make the *native* corrade-rc getting found. Corrade
+rem should not cross-compile it on this platform, because then it'd get found
+rem instead of the native version with no way to distinguish the two, and all
+rem hell breaks loose. Thus also not passing CORRADE_RC_EXECUTABLE anywhere
+rem below to ensure this doesn't regress.
 set PATH=%APPVEYOR_BUILD_FOLDER%\deps-native\bin;%PATH%
 
 rem Build native corrade-rc
@@ -16,14 +21,13 @@ cmake .. ^
 cmake --build . --target install || exit /b
 cd .. || exit /b
 
-rem Crosscompile. No tests because they takes ages to build, each executable is
+rem Crosscompile. No tests because they take ages to build, each executable is
 rem a msix file, and they can't be reasonably run either. F this platform.
 mkdir build-rt && cd build-rt || exit /b
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_SYSTEM_NAME=WindowsStore ^
     -DCMAKE_SYSTEM_VERSION=10.0 ^
-    -DCORRADE_RC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/deps-native/bin/corrade-rc.exe ^
     -DCORRADE_BUILD_STATIC=ON ^
     -G "%GENERATOR%" || exit /b
 cmake --build . --config Release -- /m /v:m  || exit /b
