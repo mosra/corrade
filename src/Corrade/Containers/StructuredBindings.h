311f767b9bcf7291b3939816cdc0ceeff88a2edb
Vladimír Vondruš <mosra@centrum.cz>
2025-01-01 20:07:15 +0100

    Containers: make StructuredBindings.h usable without <utility>.
    Until now it seemed like structured bindings just don't work without
    <utility> included. However, it turns out that it was just because
    <utility> provided convenience tuple_size and tuple_element overloads
    for const types that various code unknowingly relied on. Explicitly
    adding those for each type makes everything work even without <utility>.

--- src/Corrade/Containers/StructuredBindings.h
+++ src/Corrade/Containers/StructuredBindings.h
@@ -57,23 +57,35 @@ namespace std {
 
    Each of these is additionally guarded by an extra ifdef so single-header
    libraries can pick up individual bits without pulling in all other types as
-   well */
+   well.
+
+   To avoid a need for `#include <utility>`, which provides convenience
+   tuple_size and tuple_element overloads for `const T`, those are explicitly
+   added for each type. */
 
 /* Pair */
 #ifndef Corrade_Containers_StructuredBindings_Pair_h
 #define Corrade_Containers_StructuredBindings_Pair_h
 template<class F, class S> struct tuple_size<Corrade::Containers::Pair<F, S>>: integral_constant<size_t, 2> {};
+template<class F, class S> struct tuple_size<const Corrade::Containers::Pair<F, S>>: integral_constant<size_t, 2> {};
 template<class F, class S> struct tuple_element<0, Corrade::Containers::Pair<F, S>> { typedef F type; };
 template<class F, class S> struct tuple_element<1, Corrade::Containers::Pair<F, S>> { typedef S type; };
+template<size_t i, class F, class S> struct tuple_element<i, const Corrade::Containers::Pair<F, S>> {
+    typedef const typename tuple_element<i, Corrade::Containers::Pair<F, S>>::type type;
+};
 #endif
 
 /* Triple */
 #ifndef Corrade_Containers_StructuredBindings_Triple_h
 #define Corrade_Containers_StructuredBindings_Triple_h
 template<class F, class S, class T> struct tuple_size<Corrade::Containers::Triple<F, S, T>>: integral_constant<size_t, 3> {};
+template<class F, class S, class T> struct tuple_size<const Corrade::Containers::Triple<F, S, T>>: integral_constant<size_t, 3> {};
 template<class F, class S, class T> struct tuple_element<0, Corrade::Containers::Triple<F, S, T>> { typedef F type; };
 template<class F, class S, class T> struct tuple_element<1, Corrade::Containers::Triple<F, S, T>> { typedef S type; };
 template<class F, class S, class T> struct tuple_element<2, Corrade::Containers::Triple<F, S, T>> { typedef T type; };
+template<size_t i, class F, class S, class T> struct tuple_element<i, const Corrade::Containers::Triple<F, S, T>> {
+    typedef const typename tuple_element<i, Corrade::Containers::Triple<F, S, T>>::type type;
+};
 #endif
 
 /* StaticArray. Note that `size` can't be used as it may conflict with
@@ -81,7 +93,9 @@ template<class F, class S, class T> struct tuple_element<2, Corrade::Containers:
 #ifndef Corrade_Containers_StructuredBindings_StaticArray_h
 #define Corrade_Containers_StructuredBindings_StaticArray_h
 template<size_t size_, class T> struct tuple_size<Corrade::Containers::StaticArray<size_, T>>: integral_constant<size_t, size_> {};
+template<size_t size_, class T> struct tuple_size<const Corrade::Containers::StaticArray<size_, T>>: integral_constant<size_t, size_> {};
 template<size_t index, size_t size_, class T> struct tuple_element<index, Corrade::Containers::StaticArray<size_, T>> { typedef T type; };
+template<size_t index, size_t size_, class T> struct tuple_element<index, const Corrade::Containers::StaticArray<size_, T>> { typedef const T type; };
 #endif
 
 /* StaticArrayView. Note that `size` can't be used as it may conflict with
@@ -89,14 +103,18 @@ template<size_t index, size_t size_, class T> struct tuple_element<index, Corrad
 #ifndef Corrade_Containers_StructuredBindings_StaticArrayView_h
 #define Corrade_Containers_StructuredBindings_StaticArrayView_h
 template<size_t size_, class T> struct tuple_size<Corrade::Containers::StaticArrayView<size_, T>>: integral_constant<size_t, size_> {};
+template<size_t size_, class T> struct tuple_size<const Corrade::Containers::StaticArrayView<size_, T>>: integral_constant<size_t, size_> {};
 template<size_t index, size_t size_, class T> struct tuple_element<index, Corrade::Containers::StaticArrayView<size_, T>> { typedef T type; };
+template<size_t index, size_t size_, class T> struct tuple_element<index, const Corrade::Containers::StaticArrayView<size_, T>> { typedef const T type; };
 #endif
 
 /* StridedDimensions */
 #ifndef Corrade_Containers_StructuredBindings_StridedDimensions_h
 #define Corrade_Containers_StructuredBindings_StridedDimensions_h
 template<unsigned dimensions, class T> struct tuple_size<Corrade::Containers::StridedDimensions<dimensions, T>>: integral_constant<size_t, dimensions> {};
+template<unsigned dimensions, class T> struct tuple_size<const Corrade::Containers::StridedDimensions<dimensions, T>>: integral_constant<size_t, dimensions> {};
 template<size_t index, unsigned dimensions, class T> struct tuple_element<index, Corrade::Containers::StridedDimensions<dimensions, T>> { typedef T type; };
+template<size_t index, unsigned dimensions, class T> struct tuple_element<index, const Corrade::Containers::StridedDimensions<dimensions, T>> { typedef const T type; };
 #endif
 
 }
