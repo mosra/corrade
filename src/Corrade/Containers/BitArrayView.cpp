mosra/corrade

commit 78777968b13a8be69d317cd6d0a09650e79bf11b

Unify template SFINAE to the same form where possible.
It's now consistently using

    typename std::enable_if<..., int>::type = 0

instead of

    class = typename std::enable_if<...>::type

except for cases where a type is needed. Certain parts are
left untouched, especially in StridedArrayView member (function) slicing
and conversion from external types, I'll look at those separately. Also
made them all hidden from Doxygen, these things have nothing to do in
API docs.

src/Corrade/Containers/BitArrayView.h
@@ -132,8 +132,7 @@ template<class T> class BasicBitArrayView {
            constructZeroNullPointerAmbiguity() test for more info. FFS, zero as
            null pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_same<std::nullptr_t, U>::value>::type> constexpr /*implicit*/ BasicBitArrayView(U) noexcept: _data{}, _sizeOffset{} {}
-
+        template<class U, typename std::enable_if<std::is_same<std::nullptr_t, U>::value, int>::type = 0> constexpr /*implicit*/ BasicBitArrayView(U) noexcept: _data{}, _sizeOffset{} {}
         constexpr /*implicit*/ BasicBitArrayView() noexcept: _data{}, _sizeOffset{} {}
         #endif
 
@@ -150,7 +149,7 @@ template<class T> class BasicBitArrayView {
            size limits, so no size asserts */
         template<std::size_t size, class U
             #ifndef DOXYGEN_GENERATING_OUTPUT
-            , class = typename std::enable_if<std::is_convertible<U*, ErasedType*>::value>::type
+            , typename std::enable_if<std::is_convertible<U*, ErasedType*>::value, int>::type = 0
             #endif
         > constexpr /*implicit*/ BasicBitArrayView(U(&data)[size]) noexcept: _data{data}, _sizeOffset{sizeof(U)*8*size << 3} {}
 
@@ -176,12 +175,25 @@ template<class T> class BasicBitArrayView {
          */
         template<std::size_t dataSize, class U
             #ifndef DOXYGEN_GENERATING_OUTPUT
+            /* GCC doesn't like BasicBitArrayView<T>::ErasedType* used in the
+               SFINAE in the separate declaration, so I have to unwrap the
+               typedef there. OTOH MSVC 2015 cannot match the value parameter
+               at all, only a type parameter. Don't need to prevent any
+               ambiguity with this constructor so it's fine. */
+            #ifndef CORRADE_MSVC2015_COMPATIBILITY
+            , typename std::enable_if<std::is_convertible<U*, ErasedType*>::value, int>::type = 0
+            #else
             , class = typename std::enable_if<std::is_convertible<U*, ErasedType*>::value>::type
+            #endif
             #endif
         > constexpr /*implicit*/ BasicBitArrayView(U(&data)[dataSize], std::size_t offset, std::size_t size) noexcept;
 
         /** @brief Construct a @ref BitArrayView from a @ref MutableBitArrayView */
-        template<class U, class = typename std::enable_if<std::is_same<const U, T>::value>::type> constexpr /*implicit*/ BasicBitArrayView(BasicBitArrayView<U> mutable_) noexcept: _data{mutable_._data}, _sizeOffset{mutable_._sizeOffset} {}
+        template<class U
+            #ifndef DOXYGEN_GENERATING_OUTPUT
+            , typename std::enable_if<std::is_same<const U, T>::value, int>::type = 0
+            #endif
+        > constexpr /*implicit*/ BasicBitArrayView(BasicBitArrayView<U> mutable_) noexcept: _data{mutable_._data}, _sizeOffset{mutable_._sizeOffset} {}
 
         /* No bool conversion operator right now, as it's yet unclear what
            semantic should it have -- return false if it's nullptr, if the size
@@ -237,7 +249,7 @@ template<class T> class BasicBitArrayView {
          *      @ref set(std::size_t, bool) const, @ref setAll()
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void set(std::size_t i) const;
 
@@ -250,7 +262,7 @@ template<class T> class BasicBitArrayView {
          *      @ref set(std::size_t) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void setAll() const;
 
@@ -262,7 +274,7 @@ template<class T> class BasicBitArrayView {
          * @see @ref operator[](), @ref set(), @ref resetAll()
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void reset(std::size_t i) const;
 
@@ -274,7 +286,7 @@ template<class T> class BasicBitArrayView {
          * @see @ref setAll(), @ref reset(std::size_t) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void resetAll() const;
 
@@ -288,7 +300,7 @@ template<class T> class BasicBitArrayView {
          * @see @ref operator[](), @ref setAll(bool) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void set(std::size_t i, bool value) const;
 
@@ -302,7 +314,7 @@ template<class T> class BasicBitArrayView {
          * @see @ref set(std::size_t, bool) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
+        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
         #endif
         void setAll(bool value) const { value ? setAll() : resetAll(); }
 
@@ -467,8 +479,15 @@ template<class T> constexpr BasicBitArrayView<T>::BasicBitArrayView(ErasedType* 
     #endif
     size << 3 | offset)} {}
 
+#ifndef DOXYGEN_GENERATING_OUTPUT
+/* GCC doesn't like BasicBitArrayView<T>::ErasedType* used in the SFINAE, so I
+   have to unwrap the typedef here. OTOH MSVC 2015 cannot match the value
+   parameter at all, only a type parameter. Don't need to prevent any ambiguity
+   with this constructor so it's fine. */
 template<class T> template<std::size_t dataSize, class U
-    #ifndef DOXYGEN_GENERATING_OUTPUT
+    #ifndef CORRADE_MSVC2015_COMPATIBILITY
+    , typename std::enable_if<std::is_convertible<U*, typename std::conditional<std::is_const<T>::value, const void, void>::type*>::value, int>::type
+    #else
     , class
     #endif
 > constexpr BasicBitArrayView<T>::BasicBitArrayView(U(&data)[dataSize], const std::size_t offset, const std::size_t size) noexcept: BasicBitArrayView{static_cast<ErasedType*>(data), offset, (
@@ -490,25 +509,26 @@ template<class T> inline bool BasicBitArrayView<T>::operator[](std::size_t i) co
     return static_cast<T*>(_data)[((_sizeOffset & 0x07) + i) >> 3] & (1 << ((_sizeOffset + i) & 0x7));
 }
 
-template<class T> template<class U, class> inline void BasicBitArrayView<T>::set(std::size_t i) const {
+template<class T> template<class U, typename std::enable_if<!std::is_const<U>::value, int>::type> inline void BasicBitArrayView<T>::set(std::size_t i) const {
     CORRADE_DEBUG_ASSERT(i < (_sizeOffset >> 3),
         "Containers::BitArrayView::set(): index" << i << "out of range for" << (_sizeOffset >> 3) << "bits", );
     static_cast<T*>(_data)[((_sizeOffset & 0x07) + i) >> 3] |= (1 << ((_sizeOffset + i) & 0x07));
 }
 
-template<class T> template<class U, class> inline void BasicBitArrayView<T>::reset(std::size_t i) const {
+template<class T> template<class U, typename std::enable_if<!std::is_const<U>::value, int>::type> inline void BasicBitArrayView<T>::reset(std::size_t i) const {
     CORRADE_DEBUG_ASSERT(i < (_sizeOffset >> 3),
         "Containers::BitArrayView::reset(): index" << i << "out of range for" << (_sizeOffset >> 3) << "bits", );
     static_cast<T*>(_data)[((_sizeOffset & 0x07) + i) >> 3] &= ~(1 << ((_sizeOffset + i) & 0x07));
 }
 
-template<class T> template<class U, class> inline void BasicBitArrayView<T>::set(std::size_t i, bool value) const {
+template<class T> template<class U, typename std::enable_if<!std::is_const<U>::value, int>::type> inline void BasicBitArrayView<T>::set(std::size_t i, bool value) const {
     CORRADE_DEBUG_ASSERT(i < (_sizeOffset >> 3),
         "Containers::BitArrayView::set(): index" << i << "out of range for" << (_sizeOffset >> 3) << "bits", );
     /* http://graphics.stanford.edu/~seander/bithacks.html#ConditionalSetOrClearBitsWithoutBranching */
     char& byte = static_cast<T*>(_data)[((_sizeOffset & 0x07) + i) >> 3];
     byte ^= (-char(value) ^ byte) & (1 << ((_sizeOffset + i) & 0x07));
 }
+#endif
 
 template<class T> inline BasicBitArrayView<T> BasicBitArrayView<T>::slice(const std::size_t begin, const std::size_t end) const {
     CORRADE_DEBUG_ASSERT(begin <= end && end <= (_sizeOffset >> 3),

src/Corrade/Containers/StringView.h
@@ -369,8 +369,7 @@ BasicStringView {
            constructZeroNullPointerAmbiguity() test for more info. FFS, zero as
            null pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_same<std::nullptr_t, U>::value>::type> constexpr /*implicit*/ BasicStringView(U) noexcept: _data{}, _sizePlusFlags{std::size_t(StringViewFlag::Global)} {}
-
+        template<class U, typename std::enable_if<std::is_same<std::nullptr_t, U>::value, int>::type = 0> constexpr /*implicit*/ BasicStringView(U) noexcept: _data{}, _sizePlusFlags{std::size_t(StringViewFlag::Global)} {}
         constexpr /*implicit*/ BasicStringView() noexcept: _data{}, _sizePlusFlags{std::size_t(StringViewFlag::Global)} {}
         #endif
 
@@ -430,7 +429,11 @@ BasicStringView {
          * null-terminated view with @ref String::nullTerminatedView() or
          * @ref String::nullTerminatedGlobalView().
          */
-        template<class U = T, class = typename std::enable_if<std::is_const<U>::value>::type> /*implicit*/ BasicStringView(const String& data) noexcept;
+        template<class U = T
+            #ifndef DOXYGEN_GENERATING_OUTPUT
+            , typename std::enable_if<std::is_const<U>::value, int>::type = 0
+            #endif
+        > /*implicit*/ BasicStringView(const String& data) noexcept;
 
         #ifndef CORRADE_SINGLES_NO_ADVANCED_STRING_APIS
         /**
@@ -465,7 +468,11 @@ BasicStringView {
         #endif
 
         /** @brief Construct a @ref StringView from a @ref MutableStringView */
-        template<class U, class = typename std::enable_if<std::is_same<const U, T>::value>::type> constexpr /*implicit*/ BasicStringView(BasicStringView<U> mutable_) noexcept: _data{mutable_._data}, _sizePlusFlags{mutable_._sizePlusFlags} {}
+        template<class U
+            #ifndef DOXYGEN_GENERATING_OUTPUT
+            , typename std::enable_if<std::is_same<const U, T>::value, int>::type = 0
+            #endif
+        > constexpr /*implicit*/ BasicStringView(BasicStringView<U> mutable_) noexcept: _data{mutable_._data}, _sizePlusFlags{mutable_._sizePlusFlags} {}
 
         /**
          * @brief Construct from a null-terminated C string
@@ -492,7 +499,7 @@ BasicStringView {
            constructZeroNullPointerAmbiguity() test for more info. FFS, zero as
            null pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_pointer<U>::value && std::is_convertible<const U&, T*>::value>::type> /*implicit*/ BasicStringView(U data, StringViewFlags extraFlags = {}) noexcept: BasicStringView{data, extraFlags, nullptr} {}
+        template<class U, typename std::enable_if<std::is_pointer<U>::value && std::is_convertible<const U&, T*>::value, int>::type = 0> /*implicit*/ BasicStringView(U data, StringViewFlags extraFlags = {}) noexcept: BasicStringView{data, extraFlags, nullptr} {}
         #endif
 
         /**
@@ -646,7 +653,7 @@ BasicStringView {
         /* To avoid ambiguity when calling sliceSize(0, ...). FFS, zero as null
            pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value>::type> constexpr BasicStringView<T> sliceSize(U begin, std::size_t size) const {
+        template<class U, typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> constexpr BasicStringView<T> sliceSize(U begin, std::size_t size) const {
             return slice(begin, begin + size);
         }
         #endif
@@ -669,7 +676,7 @@ BasicStringView {
         #else
         /* To avoid ambiguity when calling prefix(0). FFS, zero as null pointer
            was deprecated in C++11 already, why is this still a problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value>::type> constexpr BasicStringView<T> prefix(U end) const {
+        template<class U, typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> constexpr BasicStringView<T> prefix(U end) const {
             return static_cast<T*>(end) ? slice(_data, end) : BasicStringView<T>{};
         }
         #endif
@@ -959,7 +966,7 @@ BasicStringView {
          */
         BasicStringView<T> exceptPrefix(char prefix) const = delete;
         #else
-        template<class = typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type> BasicStringView<T> exceptPrefix(T&& prefix) const = delete;
+        template<typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value, int>::type = 0> BasicStringView<T> exceptPrefix(T&& prefix) const = delete;
         #endif
 
         #ifdef CORRADE_BUILD_DEPRECATED
@@ -997,7 +1004,7 @@ BasicStringView {
          */
         BasicStringView<T> exceptSuffix(char suffix) const = delete;
         #else
-        template<class = typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value>::type> BasicStringView<T> exceptSuffix(T&& suffix) const = delete;
+        template<typename std::enable_if<std::is_same<typename std::decay<T>::type, char>::value, int>::type = 0> BasicStringView<T> exceptSuffix(T&& suffix) const = delete;
         #endif
 
         #ifdef CORRADE_BUILD_DEPRECATED
@@ -1345,7 +1352,7 @@ BasicStringView {
            for details; arguments in a flipped order to avoid accidental
            ambiguity. The ArrayView type is a template to avoid having to
            include ArrayView.h. */
-        template<class U, class = typename std::enable_if<std::is_same<T, U>::value>::type> constexpr explicit BasicStringView(StringViewFlags flags, ArrayView<U> data) noexcept: BasicStringView{data.data(), data.size(), flags} {}
+        template<class U, typename std::enable_if<std::is_same<T, U>::value, int>::type = 0> constexpr explicit BasicStringView(StringViewFlags flags, ArrayView<U> data) noexcept: BasicStringView{data.data(), data.size(), flags} {}
         #endif
 
         /* Used by the char* constructor, delinlined because it calls into

src/Corrade/Containers/AnyReference.h
@@ -82,7 +82,11 @@ template<class T> class AnyReference {
          * Expects that @p T is a base of @p U. The @ref isRvalue() state is
          * copied from @p other unchanged.
          */
-        template<class U, class = typename std::enable_if<std::is_base_of<T, U>::value>::type> /*implicit*/ AnyReference(AnyReference<U> other) noexcept: _reference{other._reference}, _isRvalue{other._isRvalue} {}
+        template<class U
+            #ifndef DOXYGEN_GENERATING_OUTPUT
+            , typename std::enable_if<std::is_base_of<T, U>::value, int>::type = 0
+            #endif
+        > /*implicit*/ AnyReference(AnyReference<U> other) noexcept: _reference{other._reference}, _isRvalue{other._isRvalue} {}
 
         /** @brief Underlying reference */
         /*implicit*/ operator T&() const { return *_reference; }

src/Corrade/Containers/Array.h
@@ -356,7 +356,7 @@ class Array {
            See the constructZeroNullPointerAmbiguity() test for more info. FFS,
            zero as null pointer was deprecated in C++11 already, why is this
            still a problem?! */
-        template<class U, class = typename std::enable_if<std::is_same<std::nullptr_t, U>::value>::type> /*implicit*/ Array(U) noexcept: _data{nullptr}, _size{0}, _deleter{} {}
+        template<class U, typename std::enable_if<std::is_same<std::nullptr_t, U>::value, int>::type = 0> /*implicit*/ Array(U) noexcept: _data{nullptr}, _size{0}, _deleter{} {}
 
         /*implicit*/ Array() noexcept: _data(nullptr), _size(0), _deleter{} {}
         #endif
@@ -656,8 +656,8 @@ class Array {
            builtin operator[] for pointers if an int or ssize_t is used due to
            the implicit pointer conversion. Sigh. */
         /** @todo clean up once implicit pointer conversion is removed */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, std::size_t>::value>::type> T& operator[](U i);
-        template<class U, class = typename std::enable_if<std::is_convertible<U, std::size_t>::value>::type> const T& operator[](U i) const;
+        template<class U, typename std::enable_if<std::is_convertible<U, std::size_t>::value, int>::type = 0> T& operator[](U i);
+        template<class U, typename std::enable_if<std::is_convertible<U, std::size_t>::value, int>::type = 0> const T& operator[](U i) const;
         #endif
 
         /**
@@ -697,7 +697,7 @@ class Array {
         /* To avoid ambiguity when calling sliceSize(0, ...). FFS, zero as null
            pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value>::type> ArrayView<T> sliceSize(U begin, std::size_t size) {
+        template<class U, typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> ArrayView<T> sliceSize(U begin, std::size_t size) {
             return ArrayView<T>{*this}.sliceSize(begin, size);
         }
         #endif
@@ -711,7 +711,7 @@ class Array {
         /* To avoid ambiguity when calling sliceSize(0, ...). FFS, zero as null
            pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value>::type> ArrayView<const T> sliceSize(const U begin, std::size_t size) const {
+        template<class U, typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> ArrayView<const T> sliceSize(const U begin, std::size_t size) const {
             return ArrayView<const T>{*this}.sliceSize(begin, size);
         }
         #endif
@@ -741,7 +741,7 @@ class Array {
         /* To avoid ambiguity when calling slice<size>(0). FFS, zero as null
            pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<std::size_t size_, class U, class = typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value>::type> StaticArrayView<size_, T> slice(U begin) {
+        template<std::size_t size_, class U, typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> StaticArrayView<size_, T> slice(U begin) {
             return ArrayView<T>(*this).template slice<size_>(begin);
         }
         #endif
@@ -752,7 +752,7 @@ class Array {
         /* To avoid ambiguity when calling slice<size>(0). FFS, zero as null
            pointer was deprecated in C++11 already, why is this still a
            problem?! */
-        template<std::size_t size_, class U, class = typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value>::type> StaticArrayView<size_, const T> slice(U begin) const {
+        template<std::size_t size_, class U, typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0> StaticArrayView<size_, const T> slice(U begin) const {
             return ArrayView<const T>(*this).template slice<size_>(begin);
         }
         #endif
@@ -811,7 +811,7 @@ class Array {
         #else
         /* To avoid ambiguity when calling prefix(0). FFS, zero as null pointer
            was deprecated in C++11 already, why is this still a problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value>::type>
+        template<class U, typename std::enable_if<std::is_convertible<U, T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0>
         ArrayView<T> prefix(U end) {
             return ArrayView<T>(*this).prefix(end);
         }
@@ -822,7 +822,7 @@ class Array {
         #else
         /* To avoid ambiguity when calling prefix(0). FFS, zero as null pointer
            was deprecated in C++11 already, why is this still a problem?! */
-        template<class U, class = typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value>::type>
+        template<class U, typename std::enable_if<std::is_convertible<U, const T*>::value && !std::is_convertible<U, std::size_t>::value, int>::type = 0>
         ArrayView<const T> prefix(U end) const {
             return ArrayView<const T>(*this).prefix(end);
         }
@@ -1091,7 +1091,7 @@ template<class T, class D> inline Array<T, D>& Array<T, D>::operator=(Array<T, D
 }
 
 #ifndef DOXYGEN_GENERATING_OUTPUT
-template<class T, class D> template<class U, class> const T& Array<T, D>::operator[](const U i) const {
+template<class T, class D> template<class U, typename std::enable_if<std::is_convertible<U, std::size_t>::value, int>::type> const T& Array<T, D>::operator[](const U i) const {
     CORRADE_DEBUG_ASSERT(std::size_t(i) < _size,
         "Containers::Array::operator[](): index" << i << "out of range for" << _size << "elements", _data[0]);
     return _data[i];
@@ -1109,7 +1109,7 @@ template<class T, class D> const T& Array<T, D>::back() const {
 }
 
 #ifndef DOXYGEN_GENERATING_OUTPUT
-template<class T, class D> template<class U, class> T& Array<T, D>::operator[](const U i) {
+template<class T, class D> template<class U, typename std::enable_if<std::is_convertible<U, std::size_t>::value, int>::type> T& Array<T, D>::operator[](const U i) {
     return const_cast<T&>(static_cast<const Array<T, D>&>(*this)[i]);
 }
 #endif

commit b093c78792efc83b22bf6d21f85148e2228ed3dc

Containers: prevent mangling incompatibility between GCC and Clang.
Hah, I didn't get ABI breakages between the two in quite a while. This
is caused by the changes in 78777968b13a8be69d317cd6d0a09650e79bf11b and
these three occurences are affected because they're a SFINAE-disabled
member that's only enabled on a mutable view, and the implementation is
deinlined in the cpp file. In all other cases the implementation is in
the header so even if the mangled name is different, it doesn't really
matter as the compiler makes an inline copy anyway.

So I'm reverting those to what was there before, i.e. a template type
parameter rather than template value parameter, because it's vital to
have it possible to build with one compiler (such as a distro package)
but then allow people to use the other compiler if they want.

src/Corrade/Containers/BitArrayView.cpp
@@ -40,7 +40,8 @@
 namespace Corrade { namespace Containers {
 
 /* Yes, I'm also surprised this works. On Windows (MSVC, clang-cl and MinGw) it
-   needs an explicit export otherwise the symbol doesn't get exported. */
+   needs an explicit export otherwise the symbol doesn't get exported. See the
+   note about SFINAE mangling in the header, tho. */
 template<> template<> CORRADE_UTILITY_EXPORT void BasicBitArrayView<char>::setAll() const {
     /* If there are no bits to go through, bail. Otherwise the code touches at
        least one byte. */
@@ -81,7 +82,8 @@ template<> template<> CORRADE_UTILITY_EXPORT void BasicBitArrayView<char>::setAl
 }
 
 /* Yes, I'm also surprised this works. On Windows (MSVC, clang-cl and MinGw) it
-   needs an explicit export otherwise the symbol doesn't get exported. */
+   needs an explicit export otherwise the symbol doesn't get exported. See the
+   note about SFINAE mangling in the header, tho. */
 template<> template<> CORRADE_UTILITY_EXPORT void BasicBitArrayView<char>::resetAll() const {
     /* If there are no bits to go through, bail. Otherwise the code touches at
        least one byte. */

src/Corrade/Containers/BitArrayView.h
@@ -262,7 +262,14 @@ template<class T> class BasicBitArrayView {
          *      @ref set(std::size_t) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
+        /* typename std::enable_if<std::is_const<U>::value, int>::type = 0
+           cannot be used because GCC and Clang then have different mangling
+           for the deinlined specialization in BitArrayView.cpp, which means
+           Corrade built with GCC cannot be used with Clang and vice versa.
+           Applies to resetAll() as well but not set() / reset() as those are
+           inline in the header. Similar case is in StringView.h, along with
+           more details about how the names get mangled. */
+        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
         #endif
         void setAll() const;
 
@@ -286,7 +293,10 @@ template<class T> class BasicBitArrayView {
          * @see @ref setAll(), @ref reset(std::size_t) const
          */
         #ifndef DOXYGEN_GENERATING_OUTPUT
-        template<class U = T, typename std::enable_if<!std::is_const<U>::value, int>::type = 0>
+        /* typename std::enable_if<std::is_const<U>::value, int>::type = 0
+           cannot be used because GCC and Clang then have different mangling
+           for the deinlined specialization in BitArrayView.cpp, see above */
+        template<class U = T, class = typename std::enable_if<!std::is_const<U>::value>::type>
         #endif
         void resetAll() const;
 

src/Corrade/Containers/StringView.cpp
@@ -77,7 +77,8 @@ template<class T> BasicStringView<T>::BasicStringView(T* const data, const Strin
 template<class T> BasicStringView<T>::BasicStringView(String& string) noexcept: BasicStringView{string.data(), string.size(), string.viewFlags()} {}
 
 /* Yes, I'm also surprised this works. On Windows (MSVC, clang-cl and MinGw) it
-   needs an explicit export otherwise the symbol doesn't get exported. */
+   needs an explicit export otherwise the symbol doesn't get exported. See the
+   note about SFINAE mangling in the header, tho. */
 template<> template<> CORRADE_UTILITY_EXPORT BasicStringView<const char>::BasicStringView(const String& string) noexcept: BasicStringView{string.data(), string.size(), string.viewFlags()} {}
 
 #ifndef CORRADE_SINGLES_NO_ADVANCED_STRING_APIS

src/Corrade/Containers/StringView.h
@@ -431,7 +431,16 @@ BasicStringView {
          */
         template<class U = T
             #ifndef DOXYGEN_GENERATING_OUTPUT
-            , typename std::enable_if<std::is_const<U>::value, int>::type = 0
+            /* typename std::enable_if<std::is_const<U>::value, int>::type = 0
+               cannot be used because GCC and Clang then have different
+               mangling for the deinlined specialization in StringView.cpp,
+               which means Corrade built with GCC cannot be used with Clang and
+               vice versa. With GCC-built Corrade, Clang wants to link to
+                _ZN7Corrade10Containers15BasicStringViewIKcEC1IS2_TnNSt9enable_ifIXsr3std8
+               which c++filt cannot even demangle, the other way GCC wants
+                Corrade::Containers::BasicStringView<char const>::BasicStringView<char const, 0>(Corrade::Containers::String const&)
+               which Clang doesn't export. */
+            , class = typename std::enable_if<std::is_const<U>::value>::type
             #endif
         > /*implicit*/ BasicStringView(const String& data) noexcept;
 