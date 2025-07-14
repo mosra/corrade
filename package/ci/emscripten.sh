f37591439479a5169134f8f2eba551b95054057a
Vladimír Vondruš <mosra@centrum.cz>
2025-07-14 12:05:20 +0200

    package/ci: document why Emscripten is built as Debug.
    So I don't waste time trying to enable a Release build again in the
    future.

--- package/ci/emscripten.sh
+++ package/ci/emscripten.sh
@@ -7,6 +7,9 @@ git submodule update --init
 mkdir build-emscripten && cd build-emscripten
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE="../toolchains/generic/Emscripten-wasm.cmake" \
+    `# Building as Debug always, as Release optimizations take a long time` \
+    `# and make no sense on the CI. Thus, benchmark output will not be` \
+    `# really meaningful, but we still want to run them to catch issues.` \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_INSTALL_PREFIX=$HOME/deps \
     -DCORRADE_BUILD_TESTS=ON \
